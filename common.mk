b8589b918557d7f45a88dd06b1890d372764f773
jhp <jhp@csail.mit.edu>
2010-03-22 17:37:58 -0400

    - Added daikon.jar and dcomp_premain.jar to remove the need for a separate    checkout of daikon.
     - Default RANDOOP_HOME to the current directory which makes it unnecessary
       to have a Makefile.user under most circumstances
    
     - Use the classpath * operator when the user notes that it works to shorten
       the commands.
    
     - Removed -cp command line options where they were not necessary

--- common.mk
+++ common.mk
@@ -3,35 +3,22 @@
 -include ../Makefile.user
 -include Makefile.user
 
-ifndef RANDOOP_HOME
-$(error Variable RANDOOP_HOME not found. This variable must be defined)
-endif
-
-ifndef INV
-$(error Variable INV not found. This variable must be defined)
-endif
-
-ifndef inv
-$(error Variable "inv" not found. This variable must be defined)
-endif
-
 CLASS_DIRS := $(RANDOOP_HOME)/bin \
-				$(RANDOOP_HOME)/tests \
-				$(RANDOOP_HOME)/systemtests/clover-ant-2.3.1/lib/clover.jar \
-				$(wildcard $(RANDOOP_HOME)/lib/*.jar) \
-				$(INV)/java \
-				$(INV)/java/lib/bcel.jar \
+			  $(RANDOOP_HOME)/tests \
+			  $(RANDOOP_HOME)/systemtests/clover-ant-2.3.1/lib/clover.jar 
 
-# OLD:  (I'm not sure why, but this fails for me.  Maybe a problem with my
-# javac?  Anyway, the $(wildcard ...) above works.  -MDE)
-#				$(RANDOOP_HOME)/lib/* \
-
+# The shorter version of the classpath is much easier to read in the
+# command output than the full version.  But the * classpath operator
+# doesn't work under some (not well understood) circumstances
+ifdef CLASSPATH_SUPPORTS_STAR
+  CLASS_DIRS := $(CLASS_DIRS):$(RANDOOP_HOME)/lib/*
+else
+  CLASS_DIRS := $(CLASS_DIRS):$(wildcard $(RANDOOP_HOME)/lib/*.jar)
+endif
 
 # Convert to a standard classpath
 empty:=
 space:= $(empty) $(empty)
 export CLASSPATH :=     $(subst $(space),:,$(RANDOOP_EXTRA_CLASSPATH):$(CLASS_DIRS))
-
-#export CLASSPATH := $(RANDOOP_HOME)/bin:$(RANDOOP_HOME)/tests:$(RANDOOP_HOME)/lib/asm-2.2.1.jar:$(RANDOOP_HOME)/lib/asm-tree-2.2.1.jar:$(RANDOOP_HOME)/lib/jakarta-oro-2.0.8.jar:$(RANDOOP_HOME)/lib/junit-4.3.1.jar:$(RANDOOP_HOME)/lib/log4j-1.2.9.jar:$(RANDOOP_HOME)/systemtests/clover-ant-2.3.1/lib/clover.jar:$(RANDOOP_HOME)/lib/org.eclipse.core.contenttype_3.2.100.v20070319.jar:$(RANDOOP_HOME)/lib/org.eclipse.core.jobs_3.3.1.R33x_v20070709.jar:$(RANDOOP_HOME)/lib/org.eclipse.core.resources_3.3.1.R33x_v20080205.jar:$(RANDOOP_HOME)/lib/org.eclipse.core.runtime_3.3.100.v20070530.jar:$(RANDOOP_HOME)/lib/org.eclipse.equinox.common_3.3.0.v20070426.jar:$(RANDOOP_HOME)/lib/org.eclipse.equinox.preferences_3.2.101.R33x_v20080117.jar:$(RANDOOP_HOME)/lib/org.eclipse.jdt.core_3.3.3.v_793_R33x.jar:$(RANDOOP_HOME)/lib/org.eclipse.osgi_3.3.2.R33x_v20080105.jar:$(RANDOOP_HOME)/lib/org.eclipse.text_3.3.0.v20070606-0010.jar:$(RANDOOP_HOME)/lib/iText-2.1.1.jar:$(RANDOOP_HOME)/lib/jcommon-1.0.13.jar:$(RANDOOP_HOME)/lib/jfreechart-1.0.10-swt.jar:$(RANDOOP_HOME)/lib/jfreechart-1.0.10.jar:$(RANDOOP_HOME)/lib/servlet.jar:$(RANDOOP_HOME)/lib/swtgraphics2d.jar:$(CLASSPATH):$(INV)/java:$(INV)/java/lib/bcel.jar
 
 XMXHEAP := -Xmx1650m 
