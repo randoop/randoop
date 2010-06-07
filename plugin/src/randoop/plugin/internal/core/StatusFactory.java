49486c0c52bdcfd8e4d0213fbcddf07d8ccc062a
Peter Kalauskas <pkalauskas@gmail.com>
2010-06-07 00:38:50 -0400

    Updated documention, enabled Remove button in the statements tab, renamed various classes and packages

--- plugin/src/randoop/plugin/internal/core/StatusFactory.java
+++ plugin/src/randoop/plugin/internal/core/StatusFactory.java
@@ -3,23 +3,48 @@ package randoop.plugin.internal.core;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 
-import randoop.plugin.RandoopActivator;
-import randoop.plugin.launching.IRandoopLaunchConfigConstants;
+import randoop.plugin.RandoopPlugin;
+import randoop.plugin.internal.IConstants;
 
+/**
+ * Factory class to create <code>IStatus</code> objects.
+ * 
+ * @author Peter Kalauskas
+ */
 public class StatusFactory {
+
+  /**
+   * Returns an OK status with an empty message.
+   */
   public static IStatus createOkStatus() {
-    return createOkStatus(IRandoopLaunchConfigConstants.EMPTY_STRING);
+    return createOkStatus(IConstants.EMPTY_STRING);
   }
-  
+
+  /**
+   * An OK status with the specified message.
+   * 
+   * @param message
+   *          message to be used for the returned <code>IStatus</code>
+   * @return
+   */
   public static IStatus createOkStatus(String message) {
-    return new Status(IStatus.OK, RandoopActivator.getPluginId(), message);
+    return new Status(IStatus.OK, RandoopPlugin.getPluginId(), message);
   }
-  
+
+  /**
+   * Returns an ERROR status with an empty message.
+   */
   public static IStatus createErrorStatus() {
-    return createErrorStatus(IRandoopLaunchConfigConstants.EMPTY_STRING);
+    return createErrorStatus(IConstants.EMPTY_STRING);
   }
-  
+
+  /**
+   * 
+   * @param message
+   *          message to be used for the returned <code>IStatus</code>
+   * @return
+   */
   public static IStatus createErrorStatus(String message) {
-    return new Status(IStatus.ERROR, RandoopActivator.getPluginId(), message);
+    return new Status(IStatus.ERROR, RandoopPlugin.getPluginId(), message);
   }
 }
