9a8abe73188b109c070e64d49becb1852c13a4eb
Peter Kalauskas <pkalauskas@gmail.com>
2010-07-15 00:17:31 -0400

    Implemented button to add classes from referenced classpaths; implemented refactoring of classpaths.

--- plugin/src/randoop/plugin/internal/ui/refactoring/LaunchConfigurationClasspathEntryRenameParticipant.java
+++ plugin/src/randoop/plugin/internal/ui/refactoring/LaunchConfigurationClasspathEntryRenameParticipant.java
@@ -1,47 +1,58 @@
 package randoop.plugin.internal.ui.refactoring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
 import org.eclipse.ltk.core.refactoring.participants.RenameParticipant;
-import org.eclipse.swtbot.swt.finder.utils.internal.Assert;
 
 public class LaunchConfigurationClasspathEntryRenameParticipant extends RenameParticipant {
-
+  IPath fOldPath;
+  
   @Override
   protected boolean initialize(Object element) {
-    if(element instanceof IFile) {
-      
-    } else if (element instanceof IFolder) {
-      
+    if(element instanceof IFile || element instanceof IFolder) {
+      fOldPath = ((IResource) element).getFullPath();
+      return true;
     }
     
     return false;
   }
-
+  
   @Override
   public String getName() {
-    // TODO Auto-generated method stub
-    return null;
+    return "Launch configuration participant";
   }
 
   @Override
-  public RefactoringStatus checkConditions(IProgressMonitor pm, CheckConditionsContext context)
-      throws OperationCanceledException {
-    // TODO Auto-generated method stub
-    return null;
+  public RefactoringStatus checkConditions(IProgressMonitor pm, CheckConditionsContext context) throws OperationCanceledException {
+    // return OK status
+    return new RefactoringStatus();
   }
 
   @Override
   public Change createChange(IProgressMonitor pm) throws CoreException, OperationCanceledException {
-    // TODO Auto-generated method stub
-    return null;
+    List<Change> changes = new ArrayList<Change>();
+    ILaunchConfiguration[] configs = RandoopRefactoringUtil.getRandoopTypeLaunchConfigurations();
+
+    for (ILaunchConfiguration config : configs) {
+      String newFileName = getArguments().getNewName();
+      IPath newPath = fOldPath.removeLastSegments(1).append(newFileName);
+      Change c = new LaunchConfigurationClasspathEntryChange(config, fOldPath, newPath);
+      changes.add(c);
+    }
+    
+    return RandoopRefactoringUtil.createChangeFromList(changes, "Launch configuration updates");
   }
 
 }
--- plugin/src/randoop/plugin/internal/ui/views/RandoopErrors.java
+++ plugin/src/randoop/plugin/internal/ui/views/RandoopErrors.java
@@ -9,6 +9,7 @@ import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -178,7 +179,7 @@ public class RandoopErrors implements ITreeContentProvider, IDoubleClickListener
   
   private IPath getOutputDir() {
     ILaunchConfiguration iconfig = viewPart.launch.getLaunchConfiguration();
-    RandoopArgumentCollector args = new RandoopArgumentCollector(config);
+    RandoopArgumentCollector args = new RandoopArgumentCollector(config, ResourcesPlugin.getWorkspace().getRoot());
     IPath outputDir = args.getOutputDirectory();
     return outputDir;
   }
