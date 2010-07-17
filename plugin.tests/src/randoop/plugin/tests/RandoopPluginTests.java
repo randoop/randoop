f02e4df1a0fbbba1b2dc34b03e627f5ca9ef9471
Peter Kalauskas <pkalauskas@gmail.com>
2010-07-16 20:51:32 -0400

    Fixed errors in plugin tests

--- plugin.tests/src/randoop/plugin/tests/RandoopPluginTests.java
+++ plugin.tests/src/randoop/plugin/tests/RandoopPluginTests.java
@@ -6,18 +6,18 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class RandoopPluginTests extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "randoop.tests"; //$NON-NLS-1$
 
 	// The shared instance
-	private static Activator plugin;
+	private static RandoopPluginTests plugin;
 	
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public RandoopPluginTests() {
 	}
 
 	/*
@@ -43,7 +43,7 @@ public class Activator extends AbstractUIPlugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static RandoopPluginTests getDefault() {
 		return plugin;
 	}
 
