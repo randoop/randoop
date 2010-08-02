fc188a2c59e180c36da51b47b3f2179b18e74844
Peter Kalauskas <pkalauskas@gmail.com>
2010-08-02 15:15:59 -0400

    Added classes that fail contract checks to the demo workspace

---
+++ plugin.tests/demo-workspace/Boundary/src/testables/contractChecks/ReflexivityOfEquality.java
@@ -0,0 +1,12 @@
+package testables.contracts;
+
+public class ReflexivityOfEquality {
+  public ReflexivityOfEquality() {
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    return false;
+  }
+
+}
