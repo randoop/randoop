83e69a8b9fd41e0d402ee4d17c3ff9f24bf37a74
Peter Kalauskas <pkalauskas@gmail.com>
2010-08-05 15:25:43 -0400

    Added progress monitor to launch shortcut so it is apparent that the plugin did not crash.

--- plugin/src/randoop/plugin/internal/ui/wizards/RandoopLaunchConfigurationWizard.java
+++ plugin/src/randoop/plugin/internal/ui/wizards/RandoopLaunchConfigurationWizard.java
@@ -1,11 +1,18 @@
 package randoop.plugin.internal.ui.wizards;
 
+import java.util.List;
+import java.util.Map;
+
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
-import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IType;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.graphics.RGB;
+
+import randoop.plugin.internal.core.MethodMnemonic;
+import randoop.plugin.internal.core.TypeMnemonic;
 
 public class RandoopLaunchConfigurationWizard extends Wizard {
   protected static final String DIALOG_SETTINGS_KEY = "RandoopWizard"; //$NON-NLS-1$
@@ -14,20 +21,21 @@ public class RandoopLaunchConfigurationWizard extends Wizard {
   OptionWizardPage fMainPage;
   OptionWizardPage fTestInputsPage;
 
-  public RandoopLaunchConfigurationWizard(IJavaProject project, IJavaElement[] elements, ILaunchConfigurationWorkingCopy config) throws CoreException {
+  public RandoopLaunchConfigurationWizard(IJavaProject javaProject, List<TypeMnemonic> types,
+      Map<TypeMnemonic, List<MethodMnemonic>> methodsByDeclaringTypes, ILaunchConfigurationWorkingCopy config) throws CoreException {
     super();
     
     fConfig = config;
     
-    fTestInputsPage = new TestInputsPage("Test Inputs", project, elements, fConfig);
-    fMainPage = new ParametersPage("Main", project, fConfig);
+    fTestInputsPage = new TestInputsPage("Test Inputs", javaProject, types, methodsByDeclaringTypes, fConfig);
+    fMainPage = new ParametersPage("Main", javaProject, fConfig);
     fMainPage.setPreviousPage(fMainPage);
     
     addPage(fTestInputsPage);
     addPage(fMainPage);
 
     setNeedsProgressMonitor(true);
-    setHelpAvailable(true);
+    setHelpAvailable(false);
 
     setTitleBarColor(new RGB(167, 215, 250));
     setWindowTitle("New Randoop Launch Configuration");
--- plugin/src/randoop/plugin/internal/ui/wizards/TestInputsPage.java
+++ plugin/src/randoop/plugin/internal/ui/wizards/TestInputsPage.java
@@ -1,30 +1,39 @@
 package randoop.plugin.internal.ui.wizards;
 
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.internal.ui.SWTFactory;
-import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IType;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 
+import randoop.plugin.internal.core.MethodMnemonic;
+import randoop.plugin.internal.core.TypeMnemonic;
 import randoop.plugin.internal.ui.options.ClassSelectorOption;
 import randoop.plugin.internal.ui.options.IOption;
 
 public class TestInputsPage extends OptionWizardPage {
   
   private IOption fTestInputSelectorOption;
-  
-  private IJavaProject fJavaProject;
-  private IJavaElement[] fElements;
 
-  protected TestInputsPage(String pageName, IJavaProject project, IJavaElement[] elements, ILaunchConfigurationWorkingCopy config) {
+  private IJavaProject fJavaProject;
+  private List<TypeMnemonic> fTypes;
+  private Map<TypeMnemonic, List<MethodMnemonic>> fMethodsByDeclaringTypes;
+
+  protected TestInputsPage(String pageName, IJavaProject project, List<TypeMnemonic> types,
+      Map<TypeMnemonic, List<MethodMnemonic>> methodsByDeclaringTypes, ILaunchConfigurationWorkingCopy config) {
     super(pageName, config);
-    
+
     setTitle("Test Inputs");
     setPageComplete(false);
 
     fJavaProject = project;
-    fElements = elements;
+    fTypes = types;
+    fMethodsByDeclaringTypes = methodsByDeclaringTypes;
   }
   
   @Override
@@ -32,7 +41,7 @@ public class TestInputsPage extends OptionWizardPage {
     Composite comp = SWTFactory.createComposite(parent, 1, 1, GridData.FILL_HORIZONTAL);
     setControl(comp);
 
-    fTestInputSelectorOption = new ClassSelectorOption(comp, getWizard().getContainer(), getBasicSelectionListener(), fJavaProject, fElements);
+    fTestInputSelectorOption = new ClassSelectorOption(comp, getWizard().getContainer(), getBasicSelectionListener(), fJavaProject, fTypes, fMethodsByDeclaringTypes);
     
     addOption(fTestInputSelectorOption);
     
