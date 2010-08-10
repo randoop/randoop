4c9fa459e62190a770e2373b34e63232a991af7d
Michael Ernst <mernst@cs.washington.edu>
2010-08-09 23:22:48 -0700

    Correct spelling error

--- src/randoop/RegressionCaptureVisitor.java
+++ src/randoop/RegressionCaptureVisitor.java
@@ -191,7 +191,7 @@ public final class RegressionCaptureVisitor implements ExecutionVisitor {
             // Don't create assertions over strings that are really long, as this
             // can cause the generate unit tests to be unreadable and/or non-compilable
             // due to Java restrictions on String constants.
-            if (!PrimitiveTypes.stringLenghtOK(str)) {
+            if (!PrimitiveTypes.stringLengthOK(str)) {
               continue;
             }
           }
