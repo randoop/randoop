ffb712d355b6bb9f09b7dda63eb9bf5e83f642b0
Ben Keller <bjkeller@cs.washington.edu>
2016-02-08 09:41:26 -0800

    Change import to java sdk

--- tests/randoop/operation/EnumReflectionTest.java
+++ tests/randoop/operation/EnumReflectionTest.java
@@ -8,6 +8,8 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -21,11 +23,9 @@ import randoop.test.PlayingCard;
 import randoop.test.SimpleEnum;
 import randoop.util.Reflection;
 
-import edu.emory.mathcs.backport.java.util.Arrays;
-
 /**
  * EnumReflectionTest consists of tests of {@link Reflection#getStatements}
- * to verify what is collected from enums and classes using enums. In particular, 
+ * to verify what is collected from enums and classes using enums. In particular,
  * want to collect enum constants, methods of enum (esp. if abstract), enums that are
  * are inner types.
  *
@@ -43,13 +43,13 @@ public class EnumReflectionTest {
     ArrayList<Class<?>> classes = new ArrayList<>();
     Class<?> se = SimpleEnum.class;
     classes.add(se);
-    
+
     @SuppressWarnings("unchecked")
-    List<Enum<?>> include = Arrays.asList(se.getEnumConstants());
+    List<Enum<?>> include = (List<Enum<?>>) Arrays.asList(se.getEnumConstants());
     @SuppressWarnings("unchecked")
     List<Method> exclude = Arrays.asList(se.getMethods());
     List<Operation> actual = OperationExtractor.getOperations(classes, null);
-    
+
     assertEquals("number of statements", include.size(), actual.size());
 
     for (Enum<?> e : include) {
@@ -59,12 +59,12 @@ public class EnumReflectionTest {
       assertFalse("method " + m.toGenericString() + " should not occur in simple enum", actual.contains(new MethodCall(m)) );
     }
   }
-  
+
   /**
    * innerEnum tests that Reflection.getStatements is collecting simple
    * enum constants from a class. Uses randoop.test.PlayingCard, which has
    * two public enums, one private enum, and one enum with package access.
-   * 
+   *
    */
   @SuppressWarnings("unchecked")
   @Test
@@ -72,24 +72,24 @@ public class EnumReflectionTest {
     ArrayList<Class<?>> classes = new ArrayList<>();
     Class<?> pc = PlayingCard.class;
     classes.add(pc);
-    
+
     List<Enum<?>> include = new ArrayList<>();
     List<Enum<?>> exclude = new ArrayList<>();
     for (Class<?> c : pc.getDeclaredClasses()) {
       int mods = c.getModifiers();
       if (c.isEnum()) {
         if (Modifier.isPublic(mods)) {
-          include.addAll(Arrays.asList(c.getEnumConstants()));
+          include.addAll((Collection<? extends Enum<?>>) Arrays.asList(c.getEnumConstants()));
         } else {
-          exclude.addAll(Arrays.asList(c.getEnumConstants()));
+          exclude.addAll((Collection<? extends Enum<?>>) Arrays.asList(c.getEnumConstants()));
         }
       }
     }
-    
-    
+
+
     List<Operation> actual = OperationExtractor.getOperations(classes, null);
     assertEquals("number of statements", include.size()+5, actual.size());
-    
+
     for (Enum<?> e : include) {
       assertTrue("enum constant " + e.name() + " should occur", actual.contains(new EnumConstant(e)));
     }
@@ -102,27 +102,27 @@ public class EnumReflectionTest {
    * valueEnum tests Reflection.getStatements for an enum with a field.
    * Uses randoop.test.Coin, which has a private int field, a private constructor,
    * and one public accessor method. Expect that should return the constants and accessor.
-   * Note that compiler ensures constructor access is at least package level. 
+   * Note that compiler ensures constructor access is at least package level.
    */
   @Test
   public void valueEnum() {
     ArrayList<Class<?>> classes = new ArrayList<>();
     Class<?> coin = Coin.class;
     classes.add(coin);
-    
+
     List<Operation> actual = OperationExtractor.getOperations(classes, null);
-    
+
     int count = 0;
     for (Object obj : coin.getEnumConstants()) {
       Enum<?> e = (Enum<?>)obj;
       assertTrue("enum constant " + e.name() + " should occur", actual.contains(new EnumConstant(e)));
       count++;
     }
-    
+
     for (Constructor<?> con : coin.getDeclaredConstructors()) {
       assertFalse("enum constructor " + con.getName() + "should not occur", actual.contains(new ConstructorCall(con)));
     }
-    
+
     for (Method m : coin.getMethods()) {
       if (m.getName().equals("value")) {
         assertTrue("enum method " + m.toGenericString() + " should occur", actual.contains(new MethodCall(m)));
@@ -133,11 +133,11 @@ public class EnumReflectionTest {
     }
     assertEquals("number of statements", count, actual.size());
   }
-  
+
   /**
    * abstractMethodEnum tests Reflection.getStatements for an enum with an abstract method
    * and overridden Object methods.
-   * Uses randoop.test.Operator that has four constants, one abstract method eval and each 
+   * Uses randoop.test.Operator that has four constants, one abstract method eval and each
    * constant implements eval and toString.
    */
   @Test
@@ -145,9 +145,9 @@ public class EnumReflectionTest {
     ArrayList<Class<?>> classes = new ArrayList<>();
     Class<?> op = OperatorEnum.class;
     classes.add(op);
-    
+
     List<Operation> actual = OperationExtractor.getOperations(classes, null);
-    
+
     Set<String> overrides = new TreeSet<String>();
     int count = 0;
     for (Object obj : op.getEnumConstants()) {
@@ -157,9 +157,9 @@ public class EnumReflectionTest {
       for (Method m : e.getClass().getDeclaredMethods()) {
         overrides.add(m.getName());
       }
-      
+
     }
-    
+
     for (Method m : op.getMethods()) {
       if (overrides.contains(m.getName())) {
         assertTrue("enum method " + m.toGenericString() + " should occur", actual.contains(new MethodCall(m)));
@@ -168,9 +168,9 @@ public class EnumReflectionTest {
         assertFalse("enum method " + m.toGenericString() + " should not occur", actual.contains(new MethodCall(m)));
       }
     }
-    
+
     assertEquals("number of statements", count, actual.size());
   }
-  
-  
+
+
 }
