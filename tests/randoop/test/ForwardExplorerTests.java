0cb892354e72827a28c327021a2cab5b80a11f13
Ben Keller <bjkeller@cs.washington.edu>
2016-02-10 15:28:56 -0800

    Change forward generator tests to use gentests methods

--- tests/randoop/test/ForwardExplorerTests.java
+++ tests/randoop/test/ForwardExplorerTests.java
@@ -1,22 +1,18 @@
 package randoop.test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
+import randoop.BugInRandoopException;
 import randoop.ComponentManager;
-import randoop.EqualsHashcode;
-import randoop.EqualsReflexive;
-import randoop.EqualsSymmetric;
-import randoop.EqualsToNullRetFalse;
-import randoop.ObjectContract;
 import randoop.SeedSequences;
 import randoop.main.GenInputsAbstract;
-import randoop.main.GenInputsAbstract.BehaviorType;
+import randoop.main.GenTests;
+import randoop.operation.ConstructorCall;
 import randoop.operation.Operation;
 import randoop.reflection.OperationExtractor;
 import randoop.reflection.PublicVisibilityPredicate;
-import randoop.reflection.VisibilityPredicate;
 import randoop.sequence.ExecutableSequence;
 import randoop.sequence.ForwardGenerator;
 import randoop.sequence.Sequence;
@@ -26,8 +22,6 @@ import randoop.test.bh.Cell;
 import randoop.test.bh.MathVector;
 import randoop.test.bh.Node;
 import randoop.test.bh.Tree;
-import randoop.test.predicate.ExceptionBehaviorPredicate;
-import randoop.test.predicate.ExceptionPredicate;
 import randoop.util.ReflectionExecutor;
 import randoop.util.predicate.Predicate;
 
@@ -37,14 +31,16 @@ public class ForwardExplorerTests extends TestCase {
 
   public static void test1() {
 
+    Set<Class<?>> classes = new LinkedHashSet<>();
+    classes.add(Long.class);
     List<Operation> model =
-      OperationExtractor.getOperations(Arrays.<Class<?>>asList(Long.class), null);
+      OperationExtractor.getOperations(classes, null);
     assertTrue("model not empty", model.size() != 0);
     GenInputsAbstract.dontexecute = true; // FIXME make this an instance field?
     ComponentManager mgr = new ComponentManager(SeedSequences.defaultSeeds());
     ForwardGenerator explorer = new ForwardGenerator(model,
       Long.MAX_VALUE, 5000, 5000, mgr, null, null);
-    explorer.addTestCheckGenerator(createChecker());
+    explorer.addTestCheckGenerator(createChecker(classes));
     explorer.addTestPredicate(createOutputTest());
     explorer.explore();
     GenInputsAbstract.dontexecute = false;
@@ -59,7 +55,7 @@ public class ForwardExplorerTests extends TestCase {
     boolean swapright = false;
     boolean random = false;
 
-    List<Class<?>> classes = new ArrayList<Class<?>>();
+    Set<Class<?>> classes = new LinkedHashSet<Class<?>>();
     classes.add(randoop.test.BiSortVal.class);
     classes.add(BiSort.class);
     //GenFailures.noprogressdisplay = true;
@@ -71,7 +67,7 @@ public class ForwardExplorerTests extends TestCase {
     assertTrue("model should not be empty", model.size() != 0);
     ForwardGenerator exp =
       new ForwardGenerator(model, Long.MAX_VALUE, 200, 200, mgr, null, null);
-    exp.addTestCheckGenerator(createChecker());
+    exp.addTestCheckGenerator(createChecker(classes));
     exp.addTestPredicate(createOutputTest());
     exp.explore();
     ReflectionExecutor.timeout = oldTimeout;
@@ -102,7 +98,7 @@ public class ForwardExplorerTests extends TestCase {
     boolean node = false;
     boolean tree = false;
 
-    List<Class<?>> classes = new ArrayList<Class<?>>();
+    Set<Class<?>> classes = new LinkedHashSet<Class<?>>();
     classes.add(BH.class);
     classes.add(Body.class);
     classes.add(Cell.class);
@@ -117,8 +113,8 @@ public class ForwardExplorerTests extends TestCase {
     assertTrue("model should not be empty", model.size() != 0);
     ForwardGenerator exp =
       new ForwardGenerator(model, Long.MAX_VALUE, 200, 200, mgr, null, null);
-    GenInputsAbstract.null_ratio = 0.05; //used to be forbid_null = false
-    exp.addTestCheckGenerator(createChecker());
+    GenInputsAbstract.forbid_null = false;
+    exp.addTestCheckGenerator(createChecker(classes));
     exp.addTestPredicate(createOutputTest());
     exp.explore();
     for (Sequence s : exp.getAllSequences()) {
@@ -137,29 +133,18 @@ public class ForwardExplorerTests extends TestCase {
     assertTrue(node);
     assertTrue(tree);
   }
-  
-  private static TestCheckGenerator createChecker() {
-    List<ObjectContract> contracts = new ArrayList<ObjectContract>();
 
-    // Now add all of Randoop's default contracts.
-    // Note: if you add to this list, also update the Javadoc for check_object_contracts.
-    contracts.add(new EqualsReflexive());
-    contracts.add(new EqualsSymmetric());
-    contracts.add(new EqualsHashcode());
-    contracts.add(new EqualsToNullRetFalse());
-    
-    ExceptionPredicate exceptionChecker = new ExceptionBehaviorPredicate(BehaviorType.ERROR);
-    
-    ContractCheckingVisitor contractChecker = new ContractCheckingVisitor(contracts,exceptionChecker);
-    VisibilityPredicate visibility = new PublicVisibilityPredicate();
-    ExceptionPredicate isExpected = new ExceptionBehaviorPredicate(BehaviorType.EXPECTED);
-    ExpectedExceptionCheckGen expectation; 
-    expectation = new ExpectedExceptionCheckGen(visibility, isExpected);
-    RegressionCaptureVisitor regressionCapture = new RegressionCaptureVisitor(expectation, true);
-    return new ExtendGenerator(contractChecker,regressionCapture);
+  private static TestCheckGenerator createChecker(Set<Class<?>> classes) {
+    return (new GenTests()).createTestCheckGenerator(new PublicVisibilityPredicate(), classes);
   }
-  
+
   private static Predicate<ExecutableSequence> createOutputTest() {
-    return new ErrorTestPredicate().or(new RegressionTestPredicate());
+    ConstructorCall objectConstructor = null;
+    try {
+      objectConstructor = ConstructorCall.createConstructorCall(Object.class.getConstructor());
+    } catch (Exception e) {
+      throw new BugInRandoopException(e); // Should never reach here!
+    }
+    return (new GenTests()).createTestOutputPredicate(objectConstructor, new LinkedHashSet<Class<?>>());
   }
 }
