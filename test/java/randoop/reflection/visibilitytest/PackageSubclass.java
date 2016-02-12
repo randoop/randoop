1936c8836eab89f2061ec8669426bfa37f0abe62
Ben Keller <bjkeller@cs.washington.edu>
2016-02-12 15:18:48 -0800

    Reorg files for gradle java layout

--- test/java/randoop/reflection/visibilitytest/PackageSubclass.java
+++
@@ -1,9 +0,0 @@
-package randoop.reflection.visibilitytest;
-
-/*
- * subclass for testing reflective collection of visibility bridge
- * in randoop.reflection.PackageVisibilityTest
- */
-public class PackageSubclass extends PackagePrivateBase {
-  //inherits public int theMethod(Object)
-}
