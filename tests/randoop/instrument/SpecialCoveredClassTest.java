81406803fd6ec7da748bb9d34affefc1f02f17b3
Ben Keller <bjkeller@cs.washington.edu>
2016-02-16 11:28:13 -0800

    Test filtering abstract methods

--- tests/randoop/instrument/SpecialCoveredClassTest.java
+++ tests/randoop/instrument/SpecialCoveredClassTest.java
@@ -1,18 +1,23 @@
 package randoop.instrument;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Test;
 
 import randoop.main.GenInputsAbstract;
 import randoop.main.GenTests;
+import randoop.operation.Operation;
+import randoop.reflection.DefaultReflectionPredicate;
+import randoop.reflection.OperationExtractor;
 import randoop.reflection.PublicVisibilityPredicate;
+import randoop.reflection.ReflectionPredicate;
 import randoop.reflection.VisibilityPredicate;
 
 /**
@@ -34,12 +39,22 @@ public class SpecialCoveredClassTest {
 
     GenTests.getClassesUnderTest(visibility, classes, coveredClasses);
 
-    assertTrue("should be no covered classes", coveredClasses.isEmpty());
+    assertTrue("should be one covered classes", coveredClasses.size() == 1);
+    for (Class<?> c : coveredClasses) {
+      assertEquals("name should be AbstractTarget", "randoop.instrument.testcase.AbstractTarget", c.getName());
+    }
 
+    assertTrue("should be three classes", classes.size() == 3);
     for (Class<?> c : classes) {
-      assertTrue("should not be abstract: " + c.getName(), ! Modifier.isAbstract(c.getModifiers()));
       assertTrue("should not be interface: " + c.getName(), ! c.isInterface());
     }
+
+    ReflectionPredicate predicate = new DefaultReflectionPredicate(GenInputsAbstract.omitmethods, omitfields, visibility);
+    List<Operation> model = OperationExtractor.getOperations(classes, predicate);
+
+    assertEquals("model should have six operations", 5, model.size());
+
+
   }
 
 }
