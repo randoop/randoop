4a20a5f0ce6322f5700418f6ccc83e69f1347352
Ben Keller <bjkeller@cs.washington.edu>
2016-03-14 09:31:54 -0700

    Still searching for timeout that allows contracts test to run on travis

--- src/systemtest/java/randoop/main/RandoopSystemTest.java
+++ src/systemtest/java/randoop/main/RandoopSystemTest.java
@@ -206,7 +206,7 @@ public class RandoopSystemTest {
     options.add("--classlist=resources/systemTest/buggyclasses.txt");
     options.add("--output-tests-serialized=" + workingPath + "/sequences_serialized.gzip");
 
-    long timeout = 22000L;
+    long timeout = 25000L;
 
     RandoopRunDescription randoopRunDesc = generateAndCompile(classpath, workingPath, packageName, regressionBasename, errorBasename, options, timeout);
 
