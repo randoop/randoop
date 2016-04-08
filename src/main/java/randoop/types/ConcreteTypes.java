911a867d896a9cd522e730c7ffbf49957181ab1e
Ben Keller <bjkeller@cs.washington.edu>
2016-04-08 13:04:36 -0700

    Move type constants to ConcreteTypes

---
+++ src/main/java/randoop/types/ConcreteTypes.java
@@ -0,0 +1,20 @@
+package randoop.types;
+
+/**
+ * Created by bjkeller on 4/8/16.
+ */
+public class ConcreteTypes {
+  public static final ConcreteType BOOLEAN_TYPE = new ConcreteSimpleType(boolean.class);
+  public static final ConcreteType BYTE_TYPE = new ConcreteSimpleType(byte.class);
+  public static final ConcreteType CHAR_TYPE = new ConcreteSimpleType(char.class);
+  public static final ConcreteType CLASS_TYPE = new ConcreteSimpleType(Class.class);
+  public static final ConcreteType DOUBLE_TYPE = new ConcreteSimpleType(double.class);
+  public static final ConcreteType FLOAT_TYPE = new ConcreteSimpleType(float.class);
+  public static final ConcreteType INT_TYPE = new ConcreteSimpleType(int.class);
+  public static final ConcreteType LONG_TYPE = new ConcreteSimpleType(long.class);
+  public static final ConcreteType OBJECT_TYPE = new ConcreteSimpleType(Object.class);
+  public static final ConcreteType SHORT_TYPE = new ConcreteSimpleType(short.class);
+  public static final ConcreteType STRING_TYPE = new ConcreteSimpleType(String.class);
+  public static final ConcreteType VOID_TYPE = new ConcreteSimpleType(void.class);
+
+}
