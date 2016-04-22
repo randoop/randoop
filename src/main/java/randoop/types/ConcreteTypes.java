2fa2d753f772c362f0f3481b237a597021cfacac
Ben Keller <bjkeller@cs.washington.edu>
2016-04-22 15:57:24 -0700

    Switch to correct specific type

--- src/main/java/randoop/types/ConcreteTypes.java
+++ src/main/java/randoop/types/ConcreteTypes.java
@@ -7,14 +7,14 @@ public class ConcreteTypes {
   public static final PrimitiveType BOOLEAN_TYPE = new PrimitiveType(boolean.class);
   public static final PrimitiveType BYTE_TYPE = new PrimitiveType(byte.class);
   public static final PrimitiveType CHAR_TYPE = new PrimitiveType(char.class);
-  public static final PrimitiveType CLASS_TYPE = new PrimitiveType(Class.class);
+  public static final ClassOrInterfaceType CLASS_TYPE = new SimpleClassOrInterfaceType(Class.class);
   public static final PrimitiveType DOUBLE_TYPE = new PrimitiveType(double.class);
   public static final PrimitiveType FLOAT_TYPE = new PrimitiveType(float.class);
   public static final PrimitiveType INT_TYPE = new PrimitiveType(int.class);
   public static final PrimitiveType LONG_TYPE = new PrimitiveType(long.class);
-  public static final PrimitiveType OBJECT_TYPE = new PrimitiveType(Object.class);
+  public static final ClassOrInterfaceType OBJECT_TYPE = new SimpleClassOrInterfaceType(Object.class);
   public static final PrimitiveType SHORT_TYPE = new PrimitiveType(short.class);
-  public static final PrimitiveType STRING_TYPE = new PrimitiveType(String.class);
+  public static final ClassOrInterfaceType STRING_TYPE = new SimpleClassOrInterfaceType(String.class);
   public static final PrimitiveType VOID_TYPE = new PrimitiveType(void.class);
 
 }
