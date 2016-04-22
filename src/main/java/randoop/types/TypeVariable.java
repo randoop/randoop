50877cc35f4686d6d975bdaa0bad201558ff3726
Ben Keller <bjkeller@cs.washington.edu>
2016-04-22 15:56:42 -0700

    Refine TypeVariable class

--- src/main/java/randoop/types/TypeVariable.java
+++ src/main/java/randoop/types/TypeVariable.java
@@ -1,22 +1,97 @@
 package randoop.types;
 
+import java.lang.reflect.Type;
+import java.util.Objects;
+
 /**
- * Created by bjkeller on 4/18/16.
+ * Represents a type variable used by itself as a type.
+ * (See JLS, section 4.3)
  */
 public class TypeVariable extends ReferenceType {
 
+  /** the type parameter */
   private final java.lang.reflect.TypeVariable<?> variable;
 
-  private final ParameterBound bound;
+  /** the upper typeBound on the type parameter */
+  private final ParameterBound typeBound;
 
-  public TypeVariable(java.lang.reflect.TypeVariable<?> variable, ParameterBound bound) {
+  /**
+   * Create a {@code TypeVariable} for the given type parameter
+   *
+   * @param variable  the type parameter
+   * @param bound  the upper boundon the parameter
+   */
+  private TypeVariable(java.lang.reflect.TypeVariable<?> variable, ParameterBound bound) {
     this.variable = variable;
-    this.bound = bound;
+    this.typeBound = bound;
+  }
+
+  /**
+   * {@inheritDoc}
+   * Checks that the type parameter is equal.
+   * This may be more restrictive than desired because equivalent TypeVariable
+   * objects from different instances of the same type may be distinct.
+   * @return true if the type parameters are equal, false otherwise
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof TypeVariable)) {
+      return false;
+    }
+    TypeVariable t = (TypeVariable) obj;
+    return variable.equals(t.variable);
   }
 
   @Override
-  public GeneralType apply(Substitution substitution) throws RandoopTypeException {
-    return substitution.get(variable);
+  public int hashCode() {
+    return Objects.hash(variable, typeBound);
+  }
+
+  @Override
+  public String toString() {
+    return variable.toString();
+  }
+
+  @Override
+  public GeneralType apply(Substitution substitution) {
+    GeneralType type = substitution.get(variable);
+    if (type != null) {
+      return type;
+    }
+    return this;
+  }
+
+  /**
+   * {@inheritDoc}
+   * @return name of type parameter of this type
+   */
+  @Override
+  public String getName() {
+    return variable.getName();
+  }
+
+  @Override
+  public boolean isAssignableFrom(GeneralType sourceType) {
+    return false;
+  }
+
+  public ParameterBound getTypeBound() {
+    return typeBound;
+  }
+
+  /**
+   * Creates a {@code TypeVariable} object for a given {@code java.lang.reflect.Type}
+   * reference, which must be a {@code java.lang.reflect.TypeVariable}.
+   *
+   * @param type  the type reference
+   * @return the {@code TypeVariable} for the given type
+   */
+  public static TypeVariable forType(Type type) {
+    if (! (type instanceof java.lang.reflect.TypeVariable)) {
+      throw new IllegalArgumentException("type must be a type variable");
+    }
+    java.lang.reflect.TypeVariable v = (java.lang.reflect.TypeVariable)type;
+    return new TypeVariable(v, ParameterBound.forTypes(v.getBounds()));
   }
 
 }
