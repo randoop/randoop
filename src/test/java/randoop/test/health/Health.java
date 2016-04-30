66fe3e6b83ee62acd1fb3e39d1a79489808d0016
ayman abdelghany <aymana.ghany@devfactory.com>
2016-04-30 03:16:04 +0200

    Primitives should not be boxed just for "String" conversion

--- src/test/java/randoop/test/health/Health.java
+++ src/test/java/randoop/test/health/Health.java
@@ -80,19 +80,19 @@ public class Health {
       // check for options that require arguments
       if (arg.equals("-l")) {
         if (i < args.length) {
-          maxLevel = new Integer(args[i++]).intValue();
+          maxLevel = Integer.parseInt(args[i++]);
         } else {
           throw new Error("-l requires the number of levels");
         }
       } else if (arg.equals("-t")) {
         if (i < args.length) {
-          maxTime = new Integer(args[i++]).intValue();
+          maxTime = Integer.parseInt(args[i++]);
         } else {
           throw new Error("-t requires the amount of time");
         }
       } else if (arg.equals("-s")) {
         if (i < args.length) {
-          seed = new Integer(args[i++]).intValue();
+          seed = Integer.parseInt(args[i++]);
         } else {
           throw new Error("-s requires a seed value");
         }
