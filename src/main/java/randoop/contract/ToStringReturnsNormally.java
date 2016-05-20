846c73b6d78209f2ff349d8d787096d5ef0572df
Waylon Huang <waylonh@cs.washington.edu>
2016-05-20 09:51:18 -0700

    Removed comment in ToStringReturnsNormally and removed checkNContracts in visitor

--- src/main/java/randoop/contract/ToStringReturnsNormally.java
+++ src/main/java/randoop/contract/ToStringReturnsNormally.java
@@ -26,15 +26,6 @@ public final class ToStringReturnsNormally implements ObjectContract {
     Object o = objects[0];
     assert o != null;
     o.toString();
-
-    /*
-    try {
-      o.toString();
-    } catch (Exception e) {
-      return false;
-    }
-    */
-
     return true;
   }
 
