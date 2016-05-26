1c863fa8d1a0074c5fcf7b297cbc0211aae9ef4f
Ben Keller <bjkeller@cs.washington.edu>
2016-05-26 10:05:39 -0700

    Add testing of overrides of overloads in enum constant methods

--- src/test/java/randoop/reflection/OperationModelTest.java
+++ src/test/java/randoop/reflection/OperationModelTest.java
@@ -1,5 +1,6 @@
 package randoop.reflection;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -7,12 +8,16 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import randoop.ExecutionOutcome;
+import randoop.NormalExecution;
 import randoop.main.ClassNameErrorHandler;
 import randoop.main.ThrowClassNameError;
 import randoop.main.WarnOnBadClassName;
 import randoop.operation.OperationParseException;
 import randoop.operation.TypedOperation;
+import randoop.reflection.supertypetest.InheritedEnum;
 import randoop.types.ClassOrInterfaceType;
+import randoop.types.ConcreteTypes;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -136,5 +141,78 @@ public class OperationModelTest {
     assertEquals("should be no wildcard operations", 0, wildcardOpCount);
     assertEquals("all operations should be instantiated ", model.getConcreteOperations().size(), concreteOpCount);
     assertEquals("should have 21 operations", 21, model.getConcreteOperations().size());
+  }
+
+  /**
+   * Tests the case where an enum inherits a method from a class that has an overloaded
+   * method with an incompatible type.
+   */
+  @Test
+  public void testEnumOverloads() {
+    VisibilityPredicate visibilityPredicate = new PublicVisibilityPredicate();
+    ReflectionPredicate reflectionPredicate = new DefaultReflectionPredicate();
+    Set<String> classnames = new LinkedHashSet<>();
+    classnames.add("randoop.reflection.supertypetest.InheritedEnum");
+    Set<String> exercisedClassnames = new LinkedHashSet<>();
+    Set<String> methodSignatures = new LinkedHashSet<>();
+    ClassNameErrorHandler errorHandler = new ThrowClassNameError();
+    List<String> literalsFileList = new ArrayList<>();
+    OperationModel model = null;
+    try {
+      model = OperationModel.createModel(visibilityPredicate, reflectionPredicate, classnames, exercisedClassnames, methodSignatures, errorHandler, literalsFileList);
+    } catch (NoSuchMethodException e) {
+      fail("did not find method: " + e.getMessage());
+    } catch (OperationParseException e) {
+      fail("failed to parse operation: " + e.getMessage());
+    }
+    assert model != null : "model was not initialized";
+
+    List<TypedOperation> alphaOps = new ArrayList<>();
+    for (TypedOperation operation : model.getConcreteOperations()) {
+      if (operation.getName().equals("alpha")) {
+        alphaOps.add(operation);
+      }
+    }
+    assertThat("should be two operations with name alpha", alphaOps.size(), is(equalTo(2)));
+
+    for (TypedOperation operation : alphaOps) {
+      Object[] inputs = new Object[2];
+      ExecutionOutcome outcome;
+      Object value;
+      if (operation.getOutputType().equals(ConcreteTypes.STRING_TYPE)) {
+        inputs[0] = InheritedEnum.ONE;
+        inputs[1] = 1;
+        outcome = operation.execute(inputs, null);
+        assertTrue("execution should be normal", outcome instanceof NormalExecution);
+        value = ((NormalExecution)outcome).getRuntimeValue();
+        assertThat("outcome should be string \"one\"", (String)value, is(equalTo("one")));
+
+        inputs[0] = InheritedEnum.TWO;
+        inputs[1] = 1;
+        outcome = operation.execute(inputs, null);
+        assertTrue("execution should be normal", outcome instanceof NormalExecution);
+        value = ((NormalExecution)outcome).getRuntimeValue();
+        assertThat("outcome should be string \"two\"", (String)value, is(equalTo("two")));
+
+      } else if (operation.getOutputType().equals(ConcreteTypes.INT_TYPE)){
+        inputs[0] = InheritedEnum.ONE;
+        inputs[1] = "one";
+        outcome = operation.execute(inputs, null);
+        assertTrue("execution should be normal", outcome instanceof NormalExecution);
+        value = ((NormalExecution)outcome).getRuntimeValue();
+        assertThat("outcome should be string \"one\"", (int)value, is(equalTo(1)));
+
+        inputs[0] = InheritedEnum.TWO;
+        inputs[1] = "two";
+        outcome = operation.execute(inputs, null);
+        assertTrue("execution should be normal", outcome instanceof NormalExecution);
+        value = ((NormalExecution)outcome).getRuntimeValue();
+        assertThat("outcome should be string \"one\"", (int)value, is(equalTo(2)));
+      } else {
+        fail("output type should be either String or int");
+      }
+
+    }
+
   }
 }
