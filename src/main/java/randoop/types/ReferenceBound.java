9a71aa4b510a47df4f711622117813783da991d0
Ben Keller <bjkeller@cs.washington.edu>
2016-06-03 10:59:48 -0700

    Add generalized type bound for capture conversion

---
+++ src/main/java/randoop/types/ReferenceBound.java
@@ -0,0 +1,43 @@
+package randoop.types;
+
+/**
+ * Represents an upper bound on a boundType variable that is a {@link ReferenceType}.
+ * These should only occur as bounds for {@link CaptureTypeVariable} objects constructed during
+ * capture conversion.
+ */
+class ReferenceBound extends TypeBound {
+
+  /** The boundType for this bound */
+  private final ReferenceType boundType;
+
+  /**
+   * Creates a bound for the given reference boundType.
+   *
+   * @param boundType  the reference boundType
+   */
+  ReferenceBound(ReferenceType boundType) {
+    this.boundType = boundType;
+  }
+
+
+  @Override
+  public boolean isSatisfiedBy(GeneralType argType, Substitution<ReferenceType> subst) {
+    ReferenceType boundType =  this.boundType.apply(subst);
+    return boundType.isAssignableFrom(argType);
+  }
+
+  @Override
+  public boolean isSatisfiedBy(GeneralType argType) {
+    return boundType.isAssignableFrom(argType);
+  }
+
+  @Override
+  public boolean isSubtypeOf(GeneralType otherType) {
+    return boundType.isSubtypeOf(otherType);
+  }
+
+  @Override
+  public TypeBound apply(Substitution<ReferenceType> substitution) {
+    return new ReferenceBound(boundType.apply(substitution));
+  }
+}
---
+++ src/main/java/randoop/types/TypeBound.java
@@ -0,0 +1,7 @@
+package randoop.types;
+
+/**
+ * Created by bjkeller on 6/2/16.
+ */
+public class TypeBound {
+}
