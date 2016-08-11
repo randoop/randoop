51e03448b0ed0b1e8637daf5cf5a1dfc75dabf07
Michael Ernst <mernst@cs.washington.edu>
2016-08-11 16:58:11 -0700

    Comment about missing Javadoc

--- src/main/java/randoop/types/Match.java
+++ src/main/java/randoop/types/Match.java
@@ -4,6 +4,7 @@ package randoop.types;
  * Enum used to indicate whether types should be matched exactly or using widening.
  */
 public enum Match {
+  // [[Every public member should have a Javadoc comment.  Here, the documentation "using widening" would be helpful to people seeing COMPATIBLE_TYPE.]]
   EXACT_TYPE,
   COMPATIBLE_TYPE
 }
