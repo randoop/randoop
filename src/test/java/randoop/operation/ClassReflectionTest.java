f32ce65d08170408eab955b3312fe43d43ab663b
Ben Keller <bjkeller@cs.washington.edu>
2016-12-08 13:49:22 -0800

    Update for addition of visibility predicate to OperationExtractor

--- src/test/java/randoop/operation/ClassReflectionTest.java
+++ src/test/java/randoop/operation/ClassReflectionTest.java
@@ -44,7 +44,8 @@ public class ClassReflectionTest {
       Class<?> c, ReflectionPredicate predicate, VisibilityPredicate visibilityPredicate) {
     ClassOrInterfaceType classType = ClassOrInterfaceType.forClass(c);
     final Set<TypedOperation> operations = new LinkedHashSet<>();
-    OperationExtractor extractor = new OperationExtractor(classType, operations, predicate);
+    OperationExtractor extractor =
+        new OperationExtractor(classType, operations, predicate, visibilityPredicate);
     ReflectionManager manager = new ReflectionManager(visibilityPredicate);
     manager.apply(extractor, c);
     return operations;
