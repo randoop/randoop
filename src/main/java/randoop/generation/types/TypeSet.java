8bcd5f28125d97aedbac51a894d5ef19d12693f4
Ben Keller <bjkeller@cs.washington.edu>
2016-12-13 08:06:04 -0800

    Add TypeSet class and tests

---
+++ src/main/java/randoop/generation/types/TypeSet.java
@@ -0,0 +1,169 @@
+package randoop.generation.types;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import plume.UtilMDE;
+import randoop.types.ClassOrInterfaceType;
+import randoop.types.GenericClassType;
+import randoop.types.InstantiatedType;
+import randoop.types.ParameterizedType;
+import randoop.types.ReferenceType;
+import randoop.types.Type;
+
+/**
+ * A set of non-generic Types that is upward closed by supertypes:
+ * for type t in S, all supertypes s of t are in the set S.
+ */
+public class TypeSet {
+
+  /** The subset of non-parameterized types. */
+  private final Set<Type> nonParameterizedTypes;
+
+  /** The subset of parameterized types, indexed by underlying generic type */
+  private final Map<GenericClassType, Set<InstantiatedType>> parameterizedTypes;
+
+  /** The count of unique parameterized types. */
+  private int parameterizedTypeCount;
+
+  /**
+   * Creates an empty {@link TypeSet}.
+   */
+  TypeSet() {
+    nonParameterizedTypes = new LinkedHashSet<>();
+    parameterizedTypes = new HashMap<>();
+    parameterizedTypeCount = 0;
+  }
+
+  /**
+   * Creates a {@link TypeSet} containing the given nonParameterizedTypes and their supertypes.
+   *
+   * @param types the set of nonParameterizedTypes from which the {@link TypeSet} is created.
+   */
+  public TypeSet(Set<Type> types) {
+    this();
+    for (Type type : types) {
+      this.add(type);
+    }
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof TypeSet)) {
+      return false;
+    }
+    TypeSet other = (TypeSet) obj;
+    return this.nonParameterizedTypes.equals(other.nonParameterizedTypes)
+        && this.parameterizedTypes.equals(other.parameterizedTypes);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(nonParameterizedTypes, parameterizedTypes);
+  }
+
+  @Override
+  public String toString() {
+    return "{" + UtilMDE.join(new ArrayList<>(nonParameterizedTypes), ",") + "}";
+  }
+
+  /**
+   * Adds the given type and its supertypes to this set.
+   *
+   * @param type  the type to add
+   * @return true if the type is added, and false if not
+   */
+  public boolean add(Type type) {
+    assert !type.isGeneric() : "type set should only contain non-generic types";
+
+    if (type.isParameterized()) {
+      InstantiatedType parameterizedType = (InstantiatedType) type;
+      GenericClassType genericType = parameterizedType.getGenericClassType();
+      Set<InstantiatedType> types = parameterizedTypes.get(genericType);
+      if (types == null) {
+        types = new LinkedHashSet<>();
+      }
+      parameterizedTypeCount -= types.size();
+      types.add(parameterizedType);
+      parameterizedTypeCount += types.size();
+      parameterizedTypes.put(genericType, types);
+    } else {
+      nonParameterizedTypes.add(type);
+    }
+
+    if (type.isObject()) {
+      return true;
+    }
+    if (type.isClassType()) {
+      ClassOrInterfaceType classType = (ClassOrInterfaceType) type;
+      for (ClassOrInterfaceType supertype : classType.getSuperTypes()) {
+        if (!add(supertype)) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Returns the list of nonParameterizedTypes that match the given {@link ParameterizedType}.
+   *
+   * @param searchType  the type to search for matches
+   * @return the list of matching nonParameterizedTypes
+   */
+  public List<Type> match(Type searchType) {
+    List<Type> matches = new ArrayList<>();
+    if (searchType.isParameterized() || searchType.isGeneric()) {
+      ParameterizedType parameterizedSearchType = (ParameterizedType) searchType;
+      Set<InstantiatedType> types =
+          parameterizedTypes.get(parameterizedSearchType.getGenericClassType());
+      if (types == null) {
+        return new ArrayList<>();
+      }
+      if (searchType.isGeneric()) {
+        return new ArrayList<Type>(types);
+      }
+      for (InstantiatedType type : types) {
+        if (type.isInstantiationOf((ReferenceType) searchType)) {
+          matches.add(type);
+        }
+      }
+    } else if (nonParameterizedTypes.contains(searchType)) {
+      matches.add(searchType);
+    }
+
+    return matches;
+  }
+
+  /**
+   * Returns the size of this type set.
+   *
+   * @return the number of unique types in this set
+   */
+  public int size() {
+    return nonParameterizedTypes.size() + parameterizedTypeCount;
+  }
+
+  /**
+   * Indicates whether the given type is a member of this type set.
+   *
+   * @param type  the type for which to search
+   * @return true if the type is an element of this set.
+   */
+  public boolean contains(Type type) {
+    if (type.isGeneric()) {
+      return false;
+    }
+    if (type.isParameterized()) {
+      GenericClassType keyType = ((InstantiatedType) type).getGenericClassType();
+      Set<InstantiatedType> types = parameterizedTypes.get(keyType);
+      return types != null && types.contains(type);
+    }
+    return nonParameterizedTypes.contains(type);
+  }
+}
---
+++ src/test/java/randoop/generation/types/TypeSetTest.java
@@ -0,0 +1,130 @@
+package randoop.generation.types;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import randoop.types.GenericClassType;
+import randoop.types.JavaTypes;
+import randoop.types.ReferenceType;
+import randoop.types.Substitution;
+import randoop.types.Type;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class TypeSetTest {
+
+  @Test
+  public void emptySetTest() {
+    TypeSet s1 = new TypeSet();
+    TypeSet s2 = new TypeSet(new HashSet<Type>());
+
+    assertThat("empty sets should be equal", s1, is(equalTo(s2)));
+    assertThat("empty set should have zero size", s1.size(), is(equalTo(0)));
+    assertTrue(
+        "empty set should not have match for any type", s1.match(JavaTypes.OBJECT_TYPE).isEmpty());
+  }
+
+  @Test
+  public void objectSetTest() {
+    TypeSet empty = new TypeSet();
+    TypeSet s1 = new TypeSet();
+    s1.add(JavaTypes.OBJECT_TYPE);
+    Set<Type> inputTypes = new HashSet<>();
+    inputTypes.add(JavaTypes.OBJECT_TYPE);
+    TypeSet s2 = new TypeSet(inputTypes);
+    assertThat("object set should not be equal to empty set", s1, is(not(equalTo(empty))));
+    assertThat(
+        "object and empty set should have different hashcodes",
+        s1.hashCode(),
+        is(not(equalTo(empty.hashCode()))));
+    assertThat("object sets should be equal", s1, is(equalTo(s2)));
+    assertThat("object set should have size 1", s1.size(), is(equalTo(1)));
+    assertTrue(
+        "object set should have match for object",
+        s1.match(JavaTypes.OBJECT_TYPE).contains(JavaTypes.OBJECT_TYPE)
+            && s1.match(JavaTypes.OBJECT_TYPE).size() == 1);
+    assertTrue("object set should contain object", s1.contains(JavaTypes.OBJECT_TYPE));
+  }
+
+  @Test
+  public void integerSetTest() {
+    Type integerType = JavaTypes.INT_TYPE.toBoxedPrimitive();
+
+    TypeSet s = new TypeSet();
+    s.add(integerType);
+    List<Type> expected = new ArrayList<>();
+    expected.add(integerType);
+    expected.add(Type.forClass(Number.class));
+    expected.add(JavaTypes.OBJECT_TYPE);
+    expected.add(JavaTypes.SERIALIZABLE_TYPE);
+    Substitution<ReferenceType> substitution =
+        Substitution.forArgs(
+            JavaTypes.COMPARABLE_TYPE.getTypeParameters(), (ReferenceType) integerType);
+    Type integerComparableType = JavaTypes.COMPARABLE_TYPE.apply(substitution);
+    expected.add(integerComparableType);
+    checkSetContents(s, expected);
+
+    List<Type> expectedMatches = new ArrayList<>();
+    expectedMatches.add(integerComparableType);
+    checkMatches(s, JavaTypes.COMPARABLE_TYPE, expectedMatches);
+  }
+
+  @Test
+  public void integerStringSetTest() {
+    Type integerType = JavaTypes.INT_TYPE.toBoxedPrimitive();
+    TypeSet s = new TypeSet();
+    s.add(integerType);
+    s.add(JavaTypes.STRING_TYPE);
+
+    List<Type> expected = new ArrayList<>();
+    expected.add(integerType);
+    expected.add(Type.forClass(Number.class));
+    expected.add(JavaTypes.STRING_TYPE);
+    expected.add(JavaTypes.OBJECT_TYPE);
+    expected.add(JavaTypes.SERIALIZABLE_TYPE);
+    expected.add(Type.forClass(CharSequence.class));
+    Substitution<ReferenceType> substitution =
+        Substitution.forArgs(
+            JavaTypes.COMPARABLE_TYPE.getTypeParameters(), (ReferenceType) integerType);
+    Type integerComparableType = JavaTypes.COMPARABLE_TYPE.apply(substitution);
+    expected.add(integerComparableType);
+    substitution =
+        Substitution.forArgs(
+            JavaTypes.COMPARABLE_TYPE.getTypeParameters(), (ReferenceType) JavaTypes.STRING_TYPE);
+    Type stringComparableType = JavaTypes.COMPARABLE_TYPE.apply(substitution);
+    expected.add(stringComparableType);
+    checkSetContents(s, expected);
+
+    List<Type> expectedMatches = new ArrayList<>();
+    expectedMatches.add(integerComparableType);
+    expectedMatches.add(stringComparableType);
+    checkMatches(s, JavaTypes.COMPARABLE_TYPE, expectedMatches);
+  }
+
+  private void checkMatches(
+      TypeSet s, GenericClassType comparableType, List<Type> expectedMatches) {
+    List<Type> matches = s.match(JavaTypes.COMPARABLE_TYPE);
+    for (Type type : expectedMatches) {
+      assertThat("set match for " + comparableType, matches.contains(type));
+    }
+    assertThat(
+        "set should have expected match count",
+        matches.size(),
+        is(equalTo(expectedMatches.size())));
+  }
+
+  private void checkSetContents(TypeSet s, List<Type> expected) {
+    for (Type type : expected) {
+      assertTrue("set should contain " + type, s.contains(type));
+    }
+    assertThat("set should only have expected types", s.size(), is(equalTo(expected.size())));
+  }
+}
