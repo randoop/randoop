23896f6908afd99fa91c65c940c628453714968b
Michael Shintaku <michael.shintaku1@gmail.com>
2017-01-31 23:14:27 -0800

    Times in nanoseconds instead of milliseconds now.

--- src/main/java/randoop/util/WeightedRuntimeChecker.java
+++ src/main/java/randoop/util/WeightedRuntimeChecker.java
@@ -2,74 +2,82 @@ package randoop.util;
 
 public class WeightedRuntimeChecker {
 
-    // Tests gets and sets for integration
-    public static void main(String[] args) {
-        System.out.println("Values for WeightedList");
-        for (int j = 0; j < 100; j++) {
-            long res = test(new WeightedList<Object>());
-        }
-        System.out.println("Values for Update WeightedList");
-        for (int j = 0; j < 100; j++) {
-            long res = testUpdate(new WeightedBalancedTree<Object>());
-        }
-        System.out.println("Values for WeightedBalancedTree");
-
-        for (int j = 0; j < 100; j++) {
-            long res = test(new WeightedBalancedTree<Object>());
-        }
-
-        System.out.println("Values for update WeightedBalancedTree");
-        for (int j = 0; j < 100; j++) {
-            long res = testUpdate(new WeightedBalancedTree<Object>());
-        }
+  // Tests gets and sets for integration
+  public static void main(String[] args) {
+    System.out.println("Values for WeightedList");
+    long res = 0;
+    for (int j = 0; j < 100; j++) {
+      res += test(new WeightedList<Object>());
     }
+    System.out.println("WeightedList avg: " + res * 1.0 / 100);
 
-    public static long test(WeightedRandomSampler<Object> struct) {
-        Timer t = new Timer();
-        t.startTiming();
-        for (int i = 1; i < 10000; i++) {
-            int weight = i;
-            struct.add(new Object(), i);
-            WeightedElement<Object> w = struct.getRandomElement();
-        }
-        t.stopTiming();
-        System.out.println(t.getTimeElapsedMillis());
-        return t.getTimeElapsedMillis();
-        // TODO print this to a csv file.
+    System.out.println("Values for Update WeightedList");
+    res = 0;
+    for (int j = 0; j < 100; j++) {
+      res += testUpdateWeightedList(new WeightedList<Object>());
     }
+    System.out.println("WeightedList Update avg: " + res * 1.0 / 100);
 
-    public static long testUpdate(WeightedBalancedTree<Object> struct) {
-        Timer t = new Timer();
-        Object root = new Object();
-        struct.add(root, 1);
-        t.startTiming();
-        for (int i = 1; i < 10000; i++) {
-            int weight = i;
-            struct.add(new Object(), i);
-            WeightedElement<Object> w = struct.getRandomElement();
-            struct.update(root, i + 1);
-        }
-        t.stopTiming();
-        System.out.println(t.getTimeElapsedMillis());
-        return t.getTimeElapsedMillis();
-        // TODO print this to a csv file.
+    System.out.println("Values for WeightedBalancedTree");
+    res = 0;
+    for (int j = 0; j < 100; j++) {
+      res += test(new WeightedBalancedTree<Object>());
     }
+    System.out.println("WeightedBalancedTree avg: " + res * 1.0 / 100);
 
-    public static long testUpdate(WeightedList<Object> struct) {
-        Timer t = new Timer();
-        WeightedElement<Object> root = new WeightedElement<>(new Object(), 1);
-        struct.add(root);
-        t.startTiming();
-        for (int i = 1; i < 10000; i++) {
-            int weight = i;
-            struct.add(new Object(), i);
-            WeightedElement<Object> w = struct.getRandomElement();
-            root.setWeight(i + 1);
-            struct.update(root);
-        }
-        t.stopTiming();
-        System.out.println(t.getTimeElapsedMillis());
-        return t.getTimeElapsedMillis();
-        // TODO print this to a csv file.
+    System.out.println("Values for update WeightedBalancedTree");
+    res = 0;
+    for (int j = 0; j < 100; j++) {
+      res += testUpdateWeightedBalancedTree(new WeightedBalancedTree<Object>());
     }
+    System.out.println("WeightedBalancedTree Update avg: " + res * 1.0 / 100);
+  }
+
+  public static long test(WeightedRandomSampler<Object> struct) {
+    long start = System.nanoTime();
+    for (int i = 1; i < 10000; i++) {
+      int weight = i;
+      struct.add(new Object(), weight);
+      WeightedElement<Object> w = struct.getRandomElement();
+    }
+    long total = System.nanoTime() - start;
+    System.out.println(total);
+    return total;
+    // TODO print this to a csv file.
+  }
+
+  // Tests the update in WeightedBalancedTree
+  public static long testUpdateWeightedBalancedTree(WeightedBalancedTree<Object> struct) {
+    Object root = new Object();
+    struct.add(root, 1);
+    long start = System.nanoTime();
+    for (int i = 1; i < 10000; i++) {
+      int weight = i;
+      struct.add(new Object(), weight);
+      WeightedElement<Object> w = struct.getRandomElement();
+      struct.update(root, i + 1);
+    }
+    long total = System.nanoTime() - start;
+    System.out.println(total);
+    return total;
+    // TODO print this to a csv file.
+  }
+
+  // Tests the update in WeightedList
+  public static long testUpdateWeightedList(WeightedList<Object> struct) {
+    WeightedElement<Object> root = new WeightedElement<>(new Object(), 1);
+    struct.add(root);
+    long start = System.nanoTime();
+    for (int i = 1; i < 10000; i++) {
+      int weight = i;
+      struct.add(new Object(), weight);
+      WeightedElement<Object> w = struct.getRandomElement();
+      root.setWeight(i + 1);
+      struct.update(root);
+    }
+    long total = System.nanoTime() - start;
+    System.out.println(total);
+    return total;
+    // TODO print this to a csv file.
+  }
 }
