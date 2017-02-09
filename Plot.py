11686ac8f67dd7ea76f50a2fc369b9afd292d635
Jakob Sunde <jsunde@uw.edu>
2017-02-08 20:15:20 -0800

    Work on Plot.py datafile reading

--- Plot.py
+++ Plot.py
@@ -2,7 +2,7 @@
 # you will need to install it, run `pip install matplotlib`,
 # if you don't have pip, run `sudo yum install python-pip python-wheel`
 import matplotlib.pyplot as plt
-import numpy
+import numpy, re
 
 # Plots will be in the form plt.boxplot(data, labels=labels)
 # Data will be a list of lists, each inner list is the data for one
@@ -12,3 +12,34 @@ import numpy
 plt.title('Title')
 
 plt.show()
+
+projects = ['Chart', 'Math', 'Time', 'Lang']
+
+def generatePlot(fileName, project, exp, condition, metric):
+	f = open(fileName, 'r')
+
+	if exp == 'Complete':
+		pass	
+	elif exp == 'Individual':
+		data = {2:[0, 0], 10:[0, 0], 30:[0, 0], 60:[0, 0]}
+
+	lines = f.readlines()
+	time = lines[0]
+	for i in range(0, len(lines), 2):
+		if lines[i] == '\n':
+			i += 1
+			time = lines[i]
+
+			i += 1
+		
+		data[time][0] += int(lines[i])
+		data[time][1] += int(lines[i + 1])
+
+def main():
+	fileName = sys.argv[1]
+
+	# Extract infor for plot being generated from filename
+	generatePlot(fileName, re.split('[_.]', filename))
+
+if __name__ == '__main__':
+    main()
