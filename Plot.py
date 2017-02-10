443fc82f388334af9f093c996a092ab6b1d4042e
jsunde <jsunde@cs.washington.edu>
2017-02-10 11:44:28 -0800

    Got boxplots drawn using Plot.py

--- Plot.py
+++ Plot.py
@@ -2,50 +2,82 @@
 # you will need to install it, run `pip install matplotlib`,
 # if you don't have pip, run `sudo yum install python-pip python-wheel`
 import matplotlib.pyplot as plt
-import numpy, re
+import numpy, re, sys
 
-# Plots will be in the form plt.boxplot(data, labels=labels)
-# Data will be a list of lists, each inner list is the data for one
-# boxplot, and each value in the list labels, is the label for that boxplot
-
-# plt.boxplot(data, labels=labels)
-plt.title('Title')
-
-plt.show()
 
 projects = ['Chart', 'Math', 'Time', 'Lang']
+times = [50, 100, 150, 200, 250]
 
-def generatePlot(fileName, project, exp, condition, metric):
+def readData(fileName):
 	f = open(fileName, 'r')
+
+	# Extract information from filename
+	project, exp, condition, metric, ext = re.split('[_.]', fileName)
 
 	if exp == 'Complete':
 		pass	
 	elif exp == 'Individual':
 		# Store the data in the format timeLimit: [covered[], total]
-		data = {5:[numpy.array(), 0], 100:[numpy.array(), 0], 150:[numpy.array(), 0], 200:[numpy.array(), 0]}
+		data = [[] for i in range(len(times))]
 
 	lines = f.readlines()
 
-	# Set time to int in header 'TIME 5'
-	time = lines[0].split(' ')[1]
-	for i in range(len(lines)):
-		if lines[i] == '\n':
-			i += 1
-			time = lines[i]
 
-			# Set Total Lines for this time limit
-			i += 1
-			data[time][1] = int(lines[i])
+	timeIndex = 0
+	totalLines = 0
+	i = 0
+	while i < len(lines):
+		line = lines[i].lstrip().rstrip()
+		print line
+		if "TIME" in line:
+			# Set time to int in header 'TIME 5'
+			timeIndex = times.index(int(line.split(' ')[1]))
 
-		
+			i += 1
+			line = lines[i].lstrip().rstrip()
+
+		# Set totalLines for this time limit
+		totalLines = int(lines[i + 1])
+
 		# Add to lines covered
-		data[time][1].append(int(lines[i]))
+		data[timeIndex].append(float(line) * 100 / totalLines)
+
+		i+=2
+
+	title = '%s Coverage Percentage'
+	return ('L', data)
+
+def toPercent(y, position):
+	s = str(100 * y)
+
+	# The percent symbol needs escaping in latex
+	if matplotlib.rcParams['text.usetex'] is True:
+		return s + r'$\%$'
+	else:
+		return s + '%'
 
 def main():
 	fileName = sys.argv[1]
 
 	# Extract infor for plot being generated from filename
-	generatePlot(fileName, re.split('[_.]', filename))
+	data = readData(fileName)
+
+
+	# Plots will be in the form plt.boxplot(data, labels=labels)
+	# Data will be a list of lists, each inner list is the data for one
+	# boxplot, and each value in the list labels, is the label for that boxplot
+
+	# plt.boxplot(data, labels=labels)
+
+	plt.figure()
+	plt.title('Title')
+	plt.xlabel('Global Time Limit (s)')
+	plt.ylabel('Coverage (%)')
+	plt.boxplot(data)
+
+	# Set Percent Formatter
+	plt.savefig(fileName, fromat='png')
+	plt.show()
 
 if __name__ == '__main__':
     main()
