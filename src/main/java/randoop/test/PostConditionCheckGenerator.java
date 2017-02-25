3bd65fd71de7eac182ac8209e75dcc3d4a3148d9
Ben Keller <bjkeller@cs.washington.edu>
2017-02-25 09:34:19 -0800

    Add regression case for @return post-condition handling

--- src/main/java/randoop/test/PostConditionCheckGenerator.java
+++ src/main/java/randoop/test/PostConditionCheckGenerator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import randoop.ExecutionOutcome;
 import randoop.NormalExecution;
+import randoop.NotExecuted;
 import randoop.condition.Condition;
 import randoop.sequence.ExecutableSequence;
 import randoop.sequence.Variable;
@@ -40,18 +41,24 @@ public class PostConditionCheckGenerator implements TestCheckGenerator {
   public TestChecks visit(ExecutableSequence s) {
     int finalIndex = s.sequence.size() - 1;
     ExecutionOutcome result = s.getResult(finalIndex);
-    TestChecks checks = new ErrorRevealingChecks();
-    if (result instanceof NormalExecution) {
+    TestChecks checks;
+    if (result instanceof NotExecuted) {
+      throw new Error("Abnormal execution in sequence: " + s);
+    } else if (result instanceof NormalExecution) {
       ArrayList<Variable> inputs = new ArrayList<>(s.sequence.getInputs(finalIndex));
       inputs.add(s.sequence.getVariable(finalIndex));
       Object[] inputValues = s.getRuntimeInputs(inputs);
-      //inputValues[inputValues.length - 1] = ((NormalExecution) result).getRuntimeValue();
       if (s.sequence.getStatement(finalIndex).getOperation().isStatic()) {
         inputValues = addNullReceiver(inputValues);
       }
       if (!postcondition.check(inputValues)) {
-        checks.add(new PostConditionCheck(postcondition, inputs));
+        checks = new ErrorRevealingChecks();
+      } else {
+        checks = new RegressionChecks();
       }
+      checks.add(new PostConditionCheck(postcondition, inputs));
+    } else { // if execution was exceptional, return empty checks
+      checks = new ErrorRevealingChecks();
     }
     return checks;
   }
