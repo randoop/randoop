7545f706300878c541466974bd7fe2b87c85082b
Ben Keller <bjkeller@cs.washington.edu>
2017-03-21 09:49:36 -0700

    Add system test of exercised class agent
    Add system test of exercised class agent
    1. Modify system test environment to capture javaagent jar paths
    2. Modify Randoop run to use javaagent path if set
    3. Add test to run exercised class agent over input classes

---
+++ src/testinput/java/randoop/instrument/testcase/ChildOfTarget.java
@@ -0,0 +1,13 @@
+package randoop.instrument.testcase;
+
+public class ChildOfTarget extends AbstractTarget {
+
+  public ChildOfTarget(int i) {
+    super(i);
+  }
+
+  @Override
+  public void set(int i) {
+    this.i = i;
+  }
+}
