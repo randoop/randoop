56da940655498d29ec65792c58fd8f94bbd96bfc
Ben Keller <bjkeller@cs.washington.edu>
2017-05-22 14:23:57 -0700

    Add package relocation to javaagents

--- src/mapcalltest/java/randoop/instrument/CallReplacementTest.java
+++ src/mapcalltest/java/randoop/instrument/CallReplacementTest.java
@@ -2,15 +2,21 @@ package randoop.instrument;
 
 import static org.junit.Assert.assertTrue;
 
-import com.mypackage.ClassWithCalls;
 import org.junit.Test;
 
 /** Tests the mapcall agent. */
 public class MapCallTest {
+  /*
   @Test
   public void manualExampleTest() {
-    ClassWithCalls obj = new ClassWithCalls();
-    obj.exiting();
+    SystemExitClass obj = new SystemExitClass();
+    obj.checkValue(1);
     assertTrue("didn't call system.exit()", true);
+  }
+  */
+
+  @Test
+  public void dummyTest() {
+    assertTrue("this should fail", false);
   }
 }
