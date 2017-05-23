98986a9699ac3565f50b2a43b2ee7f30332c4ec3
Ben Keller <bjkeller@cs.washington.edu>
2017-05-23 09:25:28 -0700

    Add tests for mapcall agent

--- src/mapcalltest/java/randoop/instrument/CallReplacementTest.java
+++ src/mapcalltest/java/randoop/instrument/CallReplacementTest.java
@@ -2,21 +2,49 @@ package randoop.instrument;
 
 import static org.junit.Assert.assertTrue;
 
+import components.DialogDemo;
+import input.SystemExitClass;
+import javax.swing.*;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import randoop.SystemExitCalledError;
 
 /** Tests the mapcall agent. */
-public class MapCallTest {
-  /*
-  @Test
-  public void manualExampleTest() {
-    SystemExitClass obj = new SystemExitClass();
-    obj.checkValue(1);
-    assertTrue("didn't call system.exit()", true);
-  }
-  */
+public class CallReplacementTest {
+
+  @Rule public ExpectedException thrown = ExpectedException.none();
 
   @Test
-  public void dummyTest() {
-    assertTrue("this should fail", false);
+  public void systemExitTest() {
+
+    SystemExitClass obj = new SystemExitClass(5);
+    obj.checkValue(5);
+    assertTrue("this should pass", true);
+
+    int value = 0;
+    thrown.expect(SystemExitCalledError.class);
+    thrown.expectMessage("System exit with status " + value + " ignored");
+    obj.checkValue(value);
   }
+
+  /* code borrowed from {@code createAndShowGUI()} in Oracle example components.DialogDemo */
+  /*  removed until replacements can be defined */
+  @Test
+  public void swingTest() {
+    thrown = ExpectedException.none();
+
+    JFrame frame = new JFrame("TestFrame");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    //Create and set up the content pane.
+    DialogDemo newContentPane = new DialogDemo(frame);
+    newContentPane.setOpaque(true); //content panes must be opaque
+    frame.setContentPane(newContentPane);
+
+    //Display the window.
+    frame.pack();
+    frame.setVisible(true);
+  }
+  /**/
 }
