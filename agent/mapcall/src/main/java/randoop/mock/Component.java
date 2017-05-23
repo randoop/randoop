f95afcc6638b70adc1d1ee88fbfb59fbe6b6850e
Ben Keller <bjkeller@cs.washington.edu>
2017-05-23 13:43:56 -0700

    Add replacements for AWT/Swing setVisible

---
+++ agent/mapcall/src/main/java/randoop/mock/Component.java
@@ -0,0 +1,8 @@
+package randoop.mock;
+
+/** Class of mock methods for {@code java.awt.Component} */
+public class Component {
+  public static void setVisible(boolean visible) {
+    // do nothing
+  }
+}
---
+++ agent/mapcall/src/main/java/randoop/mock/JComponent.java
@@ -0,0 +1,10 @@
+package randoop.mock;
+
+/** Implements mock methods for {@code javax.swing.JComponent} */
+public class JComponent {
+
+  /** Mock method. Avoid having any {@code JComponent} make itself visible. */
+  public static void setVisible(boolean visible) {
+    // do nothing
+  }
+}
