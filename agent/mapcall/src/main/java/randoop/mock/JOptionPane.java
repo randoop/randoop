fd0c280dd322b9862af8dfcbbcb070c257f9273a
Ben Keller <bjkeller@cs.washington.edu>
2017-05-31 10:07:50 -0700

    Add mock of JOptionPane

---
+++ agent/mapcall/src/main/java/randoop/mock/JOptionPane.java
@@ -0,0 +1,142 @@
+package randoop.mock;
+
+import javax.swing.*;
+
+/** Created by bjkeller on 5/26/17. */
+public class JOptionPane {
+  public static int showConfirmDialog(Component parent, Object message) {
+    return 0;
+  }
+
+  public static int showConfirmDialog(
+      Component parent, Object message, String title, int optionType) {
+    return 0;
+  }
+
+  public static int showConfirmDialog(
+      Component parent, Object message, String title, int optionType, int messageType) {
+    return 0;
+  }
+
+  public static int showConfirmDialog(
+      Component parent, Object message, String title, int optionType, int messageType, Icon icon) {
+    return 0;
+  }
+
+  public static String showInputDialog(Object message) {
+    return "";
+  }
+
+  public static String showInputDialog(Object message, Object initialSelectionValue) {
+    return "";
+  }
+
+  public static String showInputDialog(Component parentComponent, Object message) {
+    return "";
+  }
+
+  public static String showInputDialog(
+      Component parentComponent, Object message, Object initialSelectionValue) {
+    return "";
+  }
+
+  public static String showInputDialog(
+      Component parentComponent, Object message, String title, int messageType) {
+    return "";
+  }
+
+  public static Object showInputDialog(
+      Component parentComponent,
+      Object message,
+      String title,
+      int messageType,
+      Icon icon,
+      Object[] selectionValues,
+      Object initialSelectionValue) {
+    return "";
+  }
+
+  public static void showMessageDialog(Component parentComponent, Object message) {}
+
+  public static void showMessageDialog(
+      Component parentComponent, Object message, String title, int messageType) {}
+
+  public static void showMessageDialog(
+      Component parentComponent, Object message, String title, int messageType, Icon icon) {}
+
+  public static int showOptionDialog(
+      Component parentComponent,
+      Object message,
+      String title,
+      int optionType,
+      int messageType,
+      Icon icon,
+      Object[] options,
+      Object initialValue) {
+    return 0;
+  }
+
+  public static void showInternalMessageDialog(Component parentComponent, Object message) {}
+
+  public static void showInternalMessageDialog(
+      Component parentComponent, Object message, String title, int messageType) {}
+
+  public static void showInternalMessageDialog(
+      Component parentComponent, Object message, String title, int messageType, Icon icon) {}
+
+  public static int showInternalConfirmDialog(Component parentComponent, Object message) {
+    return 0;
+  }
+
+  public static int showInternalConfirmDialog(
+      Component parentComponent, Object message, String title, int optionType) {
+    return 0;
+  }
+
+  public static int showInternalConfirmDialog(
+      Component parentComponent, Object message, String title, int optionType, int messageType) {
+    return 0;
+  }
+
+  public static int showInternalConfirmDialog(
+      Component parentComponent,
+      Object message,
+      String title,
+      int optionType,
+      int messageType,
+      Icon icon) {
+    return 0;
+  }
+
+  public static int showInternalOptionDialog(
+      Component parentComponent,
+      Object message,
+      String title,
+      int optionType,
+      int messageType,
+      Icon icon,
+      Object[] options,
+      Object initialValue) {
+    return 0;
+  }
+
+  public static String showInternalInputDialog(Component parentComponent, Object message) {
+    return "";
+  }
+
+  public static String showInternalInputDialog(
+      Component parentComponent, Object message, String title, int messageType) {
+    return "";
+  }
+
+  public static Object showInternalInputDialog(
+      Component parentComponent,
+      Object message,
+      String title,
+      int messageType,
+      Icon icon,
+      Object[] selectionValues,
+      Object initialSelectionValue) {
+    return "";
+  }
+}
