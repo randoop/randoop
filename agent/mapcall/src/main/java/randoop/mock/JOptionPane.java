dbe59ea9fa3ad0574e11fa9a8db875afd3026838
Ben Keller <bjkeller@cs.washington.edu>
2017-06-16 15:18:36 -0700

    Fix errors in default replacements and JOptionPane mock

--- agent/mapcall/src/main/java/randoop/mock/JOptionPane.java
+++ agent/mapcall/src/main/java/randoop/mock/JOptionPane.java
@@ -1,25 +1,28 @@
 package randoop.mock;
 
-import javax.swing.*;
-
 /** Created by bjkeller on 5/26/17. */
 public class JOptionPane {
-  public static int showConfirmDialog(Component parent, Object message) {
+  public static int showConfirmDialog(java.awt.Component parent, Object message) {
     return 0;
   }
 
   public static int showConfirmDialog(
-      Component parent, Object message, String title, int optionType) {
+      java.awt.Component parent, Object message, String title, int optionType) {
     return 0;
   }
 
   public static int showConfirmDialog(
-      Component parent, Object message, String title, int optionType, int messageType) {
+      java.awt.Component parent, Object message, String title, int optionType, int messageType) {
     return 0;
   }
 
   public static int showConfirmDialog(
-      Component parent, Object message, String title, int optionType, int messageType, Icon icon) {
+      java.awt.Component parent,
+      Object message,
+      String title,
+      int optionType,
+      int messageType,
+      javax.swing.Icon icon) {
     return 0;
   }
 
@@ -31,110 +34,123 @@ public class JOptionPane {
     return "";
   }
 
-  public static String showInputDialog(Component parentComponent, Object message) {
+  public static String showInputDialog(java.awt.Component parentComponent, Object message) {
     return "";
   }
 
   public static String showInputDialog(
-      Component parentComponent, Object message, Object initialSelectionValue) {
+      java.awt.Component parentComponent, Object message, Object initialSelectionValue) {
     return "";
   }
 
   public static String showInputDialog(
-      Component parentComponent, Object message, String title, int messageType) {
+      java.awt.Component parentComponent, Object message, String title, int messageType) {
     return "";
   }
 
   public static Object showInputDialog(
-      Component parentComponent,
+      java.awt.Component parentComponent,
       Object message,
       String title,
       int messageType,
-      Icon icon,
+      javax.swing.Icon icon,
       Object[] selectionValues,
       Object initialSelectionValue) {
     return "";
   }
 
-  public static void showMessageDialog(Component parentComponent, Object message) {}
+  public static void showMessageDialog(java.awt.Component parentComponent, Object message) {}
 
   public static void showMessageDialog(
-      Component parentComponent, Object message, String title, int messageType) {}
+      java.awt.Component parentComponent, Object message, String title, int messageType) {}
 
   public static void showMessageDialog(
-      Component parentComponent, Object message, String title, int messageType, Icon icon) {}
+      java.awt.Component parentComponent,
+      Object message,
+      String title,
+      int messageType,
+      javax.swing.Icon icon) {}
 
   public static int showOptionDialog(
-      Component parentComponent,
+      java.awt.Component parentComponent,
       Object message,
       String title,
       int optionType,
       int messageType,
-      Icon icon,
+      javax.swing.Icon icon,
       Object[] options,
       Object initialValue) {
     return 0;
   }
 
-  public static void showInternalMessageDialog(Component parentComponent, Object message) {}
+  public static void showInternalMessageDialog(
+      java.awt.Component parentComponent, Object message) {}
 
   public static void showInternalMessageDialog(
-      Component parentComponent, Object message, String title, int messageType) {}
+      java.awt.Component parentComponent, Object message, String title, int messageType) {}
 
   public static void showInternalMessageDialog(
-      Component parentComponent, Object message, String title, int messageType, Icon icon) {}
+      java.awt.Component parentComponent,
+      Object message,
+      String title,
+      int messageType,
+      javax.swing.Icon icon) {}
 
-  public static int showInternalConfirmDialog(Component parentComponent, Object message) {
+  public static int showInternalConfirmDialog(java.awt.Component parentComponent, Object message) {
     return 0;
   }
 
   public static int showInternalConfirmDialog(
-      Component parentComponent, Object message, String title, int optionType) {
+      java.awt.Component parentComponent, Object message, String title, int optionType) {
     return 0;
   }
 
   public static int showInternalConfirmDialog(
-      Component parentComponent, Object message, String title, int optionType, int messageType) {
+      java.awt.Component parentComponent,
+      Object message,
+      String title,
+      int optionType,
+      int messageType) {
     return 0;
   }
 
   public static int showInternalConfirmDialog(
-      Component parentComponent,
+      java.awt.Component parentComponent,
       Object message,
       String title,
       int optionType,
       int messageType,
-      Icon icon) {
+      javax.swing.Icon icon) {
     return 0;
   }
 
   public static int showInternalOptionDialog(
-      Component parentComponent,
+      java.awt.Component parentComponent,
       Object message,
       String title,
       int optionType,
       int messageType,
-      Icon icon,
+      javax.swing.Icon icon,
       Object[] options,
       Object initialValue) {
     return 0;
   }
 
-  public static String showInternalInputDialog(Component parentComponent, Object message) {
+  public static String showInternalInputDialog(java.awt.Component parentComponent, Object message) {
     return "";
   }
 
   public static String showInternalInputDialog(
-      Component parentComponent, Object message, String title, int messageType) {
+      java.awt.Component parentComponent, Object message, String title, int messageType) {
     return "";
   }
 
   public static Object showInternalInputDialog(
-      Component parentComponent,
+      java.awt.Component parentComponent,
       Object message,
       String title,
       int messageType,
-      Icon icon,
+      javax.swing.Icon icon,
       Object[] selectionValues,
       Object initialSelectionValue) {
     return "";
