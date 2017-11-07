52f34e96183392014eaa7053c847ab0ba2ef5da8
Michael Ernst <mernst@cs.washington.edu>
2017-11-07 13:55:06 -0800

    Improve constructors and printing

--- src/main/java/randoop/test/ErrorRevealingChecks.java
+++ src/main/java/randoop/test/ErrorRevealingChecks.java
@@ -1,5 +1,6 @@
 package randoop.test;
 
+import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -12,11 +13,28 @@ import java.util.Set;
  */
 public class ErrorRevealingChecks implements TestChecks<ErrorRevealingChecks> {
 
+  /** An empty set of erorr-revealing checks. */
+  public static final ErrorRevealingChecks EMPTY;
+
+  static {
+    EMPTY = new ErrorRevealingChecks();
+    EMPTY.checks = Collections.emptySet(); // make immutable
+  }
+
   private Set<Check> checks;
 
   /** Create an empty set of error checks. */
   public ErrorRevealingChecks() {
     this.checks = new LinkedHashSet<>();
+  }
+
+  /** Create a singleton set of error checks. */
+  public ErrorRevealingChecks(Check check) {
+    if (check instanceof ExceptionCheck) {
+      String msg = "No expected exceptions in error-revealing tests";
+      throw new Error(msg);
+    }
+    this.checks = Collections.<Check>singleton(check);
   }
 
   @Override
@@ -115,5 +133,10 @@ public class ErrorRevealingChecks implements TestChecks<ErrorRevealingChecks> {
   @Override
   public boolean hasInvalidBehavior() {
     return false;
+  }
+
+  @Override
+  public String toString() {
+    return this.getClass() + " of size " + checks.size() + ": " + checks.toString();
   }
 }
