eb4cd47a8e3cf13d22a8953538c95fd53d768808
Michael Ernst <mernst@cs.washington.edu>
2018-06-08 05:33:32 -0700

    Don't use WeightedElement.  Instead, make all weights explicit.

--- src/main/java/randoop/generation/SmallTestsSequenceSelection.java
+++ src/main/java/randoop/generation/SmallTestsSequenceSelection.java
@@ -1,10 +1,19 @@
 package randoop.generation;
 
+import java.util.HashMap;
+import java.util.Map;
 import randoop.sequence.Sequence;
 import randoop.util.Randomness;
 import randoop.util.SimpleList;
 
+/**
+ * Select input sequences, favoring shorter sequences. This makes Randoop produce smaller JUnit
+ * tests.
+ */
 public class SmallTestsSequenceSelection implements InputSequenceSelector {
+
+  /** Map from a sequence to its weight. */
+  private final Map<Sequence, Double> weightMap = new HashMap<>();
 
   /**
    * Pick a sequence from the candidate list using the member's natural weight.
@@ -14,6 +23,23 @@ public class SmallTestsSequenceSelection implements InputSequenceSelector {
    */
   @Override
   public Sequence selectInputSequence(SimpleList<Sequence> candidates) {
-    return Randomness.randomMemberWeighted(candidates);
+    double totalWeight = updateWeightMapForCandidates(candidates);
+    return Randomness.randomMemberWeighted(candidates, weightMap, totalWeight);
+  }
+
+  /** Update the weight for any sequence not currently in the map, and compute the total weight. */
+  private double updateWeightMapForCandidates(SimpleList<Sequence> candidates) {
+
+    double totalWeight = 0.0;
+    for (int i = 0; i < candidates.size(); i++) {
+      Sequence candidate = candidates.get(i);
+      Double weight = weightMap.get(candidate);
+      if (weight == null) {
+        weight = 1 / (double) candidate.size();
+      }
+      totalWeight += weight;
+      weightMap.put(candidate, weight);
+    }
+    return totalWeight;
   }
 }
