9fb13db8e93311d34d62b353049794865aa0e858
Michael Ernst <mernst@cs.washington.edu>
2018-07-12 14:08:18 -0700

    Change git diff command

--- .travis-build.sh
+++ .travis-build.sh
@@ -55,7 +55,8 @@ fi
 ## TODO: merge into "misc" once it is working.
 if [[ "${GROUP}" == "diff" || "${GROUP}" == "all" ]]; then
   echo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
-  (git diff $(git merge-base origin/master...HEAD) > /tmp/diff.txt 2>&1) || true
+  (git diff HEAD...$TRAVIS_BRANCH > /tmp/diff.txt 2>&1) || true
+  # (git diff $(git merge-base origin/master...HEAD) > /tmp/diff1.txt 2>&1) || true
   (./gradlew requireJavadoc > /tmp/output.txt 2>&1) || true
   ls -l /tmp
   echo "/tmp/diff.txt"
