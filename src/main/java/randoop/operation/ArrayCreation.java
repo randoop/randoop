747966837bef283ebc5b4e21d0e2018b95572fe1
Michael Ernst <mernst@alum.mit.edu>
2019-03-30 05:37:50 -0700

    Use a cast instead of toString()
    Improvement suggested by the Determinism Checker

--- src/main/java/randoop/operation/ArrayCreation.java
+++ src/main/java/randoop/operation/ArrayCreation.java
@@ -64,9 +64,10 @@ public class ArrayCreation extends CallableOperation {
   }
 
   @Override
+  // The argument array contains a single Integer.
   public ExecutionOutcome execute(Object[] input) {
     assert input.length == 1 : "requires array dimension as input";
-    int length = Integer.parseInt(input[0].toString());
+    int length = ((Integer) input[0]).intValue();
     long startTime = System.currentTimeMillis();
     Object theArray = Array.newInstance(this.componentType.getRuntimeClass(), length);
     long totalTime = System.currentTimeMillis() - startTime;
