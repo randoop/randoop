6a035bb92248ba8b4eeb2c13714d0859e71eca2f
Michael Ernst <mernst@cs.washington.edu>
2019-07-07 14:03:45 -0700

    Adapt to upstream

--- scripts/test-misc.sh
+++ scripts/test-misc.sh
@@ -22,8 +22,9 @@ echo pull request commit = `git rev-parse HEAD^2`
 # If it's a pull request, set COMMIT_RANGE and BRANCH
 if [ -n "$SYSTEM_PULLREQUEST_TARGETBRANCH" ] ; then
   ## Azure Pipelines
-  COMMIT_RANGE=`git rev-parse HEAD^1`..$BUILD_SOURCEVERSION
+  COMMIT_RANGE=`git rev-parse HEAD^1`...$BUILD_SOURCEVERSION
   BRANCH=$SYSTEM_PULLREQUEST_TARGETBRANCH
+  STRIP_LINT=4
 elif [ -n "$TRAVIS_COMMIT_RANGE" ] ; then
   ## Travis CI
   # $TRAVIS_COMMIT_RANGE is empty for builds triggered by the initial commit of a new branch.
@@ -31,6 +32,7 @@ elif [ -n "$TRAVIS_COMMIT_RANGE" ] ; then
   # good argument to `git diff` but a bad argument to `git log` (they interpret "..." differently!).
   COMMIT_RANGE=$TRAVIS_COMMIT_RANGE
   BRANCH=$CIRCLE_BRANCH
+  STRIP_LINT=2
 elif [ -n "$CIRCLE_COMPARE_URL" ] ; then
   ## CircleCI
   COMMIT_RANGE=$(echo "${CIRCLE_COMPARE_URL}" | cut -d/ -f7)
@@ -38,6 +40,8 @@ elif [ -n "$CIRCLE_COMPARE_URL" ] ; then
     COMMIT_RANGE="${COMMIT_RANGE}...${COMMIT_RANGE}"
   fi
   BRANCH=$TRAVIS_BRANCH
+  # TODO: determine correct value
+  STRIP_LINT=2
 fi
 
 echo COMMIT_RANGE=$COMMIT_RANGE
@@ -51,5 +55,5 @@ cat /tmp/diff.txt
 cat /tmp/rjp-output.txt
   [ -s /tmp/diff.txt ] || ([[ "${BRANCH}" != "master" && "${TRAVIS_EVENT_TYPE}" == "push" ]] || (echo "/tmp/diff.txt is empty for COMMIT_RANGE=$COMMIT_RANGE; try pulling base branch (often master) into compare branch (often feature branch)" && false))
   wget https://raw.githubusercontent.com/plume-lib/plume-scripts/master/lint-diff.py
-  python lint-diff.py --strip-diff=1 --strip-lint=2 /tmp/diff.txt /tmp/rjp-output.txt
+  python lint-diff.py --strip-diff=1 --strip-lint=$STRIP_LINT /tmp/diff.txt /tmp/rjp-output.txt
 fi
