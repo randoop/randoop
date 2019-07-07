ad1890bfa16e9eab918a2ffa9ff9d7b5f763716a
Michael Ernst <mernst@cs.washington.edu>
2019-07-07 13:40:51 -0700

    Improvements

--- scripts/test-misc.sh
+++ scripts/test-misc.sh
@@ -22,9 +22,9 @@ echo pull request commit = `git rev-parse HEAD^2`
 # If it's a pull request, set COMMIT_RANGE and BRANCH
 if [ -n "$SYSTEM_PULLREQUEST_TARGETBRANCH" ] ; then
   ## Azure Pipelines
-  COMMIT_RANGE=`git rev-parse HEAD^2`..$BUILD_SOURCEVERSION
+  COMMIT_RANGE=`git rev-parse HEAD^1`..$BUILD_SOURCEVERSION
   BRANCH=$SYSTEM_PULLREQUEST_TARGETBRANCH
-if [ -n "$TRAVIS_COMMIT_RANGE" ] ; then
+elif [ -n "$TRAVIS_COMMIT_RANGE" ] ; then
   ## Travis CI
   # $TRAVIS_COMMIT_RANGE is empty for builds triggered by the initial commit of a new branch.
   # Until https://github.com/travis-ci/travis-ci/issues/4596 is fixed, $TRAVIS_COMMIT_RANGE is a
@@ -44,12 +44,12 @@ echo COMMIT_RANGE=$COMMIT_RANGE
 echo BRANCH=$BRANCH
 
 if [ -n "$COMMIT_RANGE" ] ; then
-echo "COMMIT_RANGE is set"
+echo "COMMIT_RANGE is set to $COMMIT_RANGE"
   (git diff $COMMIT_RANGE > /tmp/diff.txt 2>&1) || true
 cat /tmp/diff.txt
   (./gradlew requireJavadocPrivate > /tmp/rjp-output.txt 2>&1) || true
 cat /tmp/rjp-output.txt
-  [ -s /tmp/diff.txt ] || ([[ "${BRANCH}" != "master" && "${TRAVIS_EVENT_TYPE}" == "push" ]] || (echo "/tmp/diff.txt is empty; try pulling base branch (often master) into compare branch (often feature branch)" && false))
+  [ -s /tmp/diff.txt ] || ([[ "${BRANCH}" != "master" && "${TRAVIS_EVENT_TYPE}" == "push" ]] || (echo "/tmp/diff.txt is empty for COMMIT_RANGE=$COMMIT_RANGE; try pulling base branch (often master) into compare branch (often feature branch)" && false))
   wget https://raw.githubusercontent.com/plume-lib/plume-scripts/master/lint-diff.py
   python lint-diff.py --strip-diff=1 --strip-lint=2 /tmp/diff.txt /tmp/rjp-output.txt
 fi
