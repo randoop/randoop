ccef471d1583d08d510685e98f248a8fda843eee
Michael Ernst <mernst@cs.washington.edu>
2019-07-08 10:24:51 -0700

    Use git-set-commit-range

--- scripts/test-misc.sh
+++ scripts/test-misc.sh
@@ -13,38 +13,17 @@ export SHELLOPTS
 ./gradlew checkstyle checkstyleMain checkstyleCoveredTest checkstyleReplacecallTest
 ./gradlew manual
 
-# If it's a pull request, set COMMIT_RANGE and BRANCH
-if [ -n "$SYSTEM_PULLREQUEST_TARGETBRANCH" ] ; then
-  ## Azure Pipelines
-  COMMIT_RANGE=`git rev-parse HEAD^1`...$BUILD_SOURCEVERSION
-  BRANCH=$SYSTEM_PULLREQUEST_TARGETBRANCH
-  STRIP_LINT=4
-elif [ -n "$TRAVIS_COMMIT_RANGE" ] ; then
-  ## Travis CI
-  # $TRAVIS_COMMIT_RANGE is empty for builds triggered by the initial commit of a new branch.
-  # Until https://github.com/travis-ci/travis-ci/issues/4596 is fixed, $TRAVIS_COMMIT_RANGE is a
-  # good argument to `git diff` but a bad argument to `git log` (they interpret "..." differently!).
-  COMMIT_RANGE=$TRAVIS_COMMIT_RANGE
-  BRANCH=$CIRCLE_BRANCH
-  STRIP_LINT=2
-elif [ -n "$CIRCLE_COMPARE_URL" ] ; then
-  ## CircleCI
-  COMMIT_RANGE=$(echo "${CIRCLE_COMPARE_URL}" | cut -d/ -f7)
-  if [[ $COMMIT_RANGE != *"..."* ]]; then
-    COMMIT_RANGE="${COMMIT_RANGE}...${COMMIT_RANGE}"
-  fi
-  BRANCH=$TRAVIS_BRANCH
-  # TODO: determine correct value
-  STRIP_LINT=2
-fi
+git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
+  || git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
+
+source /tmp/plume-scripts/git-set-commit-range
 
 echo COMMIT_RANGE=$COMMIT_RANGE
 echo BRANCH=$BRANCH
 
 if [ -n "$COMMIT_RANGE" ] ; then
   (git diff $COMMIT_RANGE > /tmp/diff.txt 2>&1) || true
-  (./gradlew requireJavadocPrivate > /tmp/rjp-output.txt 2>&1) || true
-  [ -s /tmp/diff.txt ] || ([[ "${BRANCH}" != "master" && "${TRAVIS_EVENT_TYPE}" == "push" ]] || (echo "/tmp/diff.txt is empty for COMMIT_RANGE=$COMMIT_RANGE; try pulling base branch (often master) into compare branch (often feature branch)" && false))
-  wget -q https://raw.githubusercontent.com/plume-lib/plume-scripts/master/lint-diff.py
-  python lint-diff.py --strip-diff=1 --strip-lint=$STRIP_LINT /tmp/diff.txt /tmp/rjp-output.txt
+  (./gradlew requireJavadocPrivate > /tmp/warnings.txt 2>&1) || true
+  [ -s /tmp/diff.txt ] || ([[ "${BRANCH}" != "master" && "${TRAVIS_EVENT_TYPE}" == "push" ]] || (echo "/tmp/diff.txt is empty for COMMIT_RANGE=$COMMIT_RANGE; try pulling base branch (often master) into compare branch (often your feature branch)" && false))
+  python /tmp/plume-scripts/lint-diff.py --guess-strip /tmp/diff.txt /tmp/warnings.txt
 fi
