0a57ebd446d427d3655381d3028a1f9a351d2d9f
Michael Ernst <mernst@cs.washington.edu>
2019-07-20 16:09:52 -0700

    Use the ci-lint-diff script

--- scripts/test-misc.sh
+++ scripts/test-misc.sh
@@ -13,17 +13,7 @@ export SHELLOPTS
 ./gradlew checkstyle checkstyleMain checkstyleCoveredTest checkstyleReplacecallTest
 ./gradlew manual
 
+(./gradlew requireJavadocPrivate > /tmp/warnings.txt 2>&1) || true
 git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
   || git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
-
-source /tmp/plume-scripts/git-set-commit-range
-
-echo COMMIT_RANGE=$COMMIT_RANGE
-echo BRANCH=$BRANCH
-
-if [ -n "$COMMIT_RANGE" ] ; then
-  (git diff $COMMIT_RANGE > /tmp/diff.txt 2>&1) || true
-  (./gradlew requireJavadocPrivate > /tmp/warnings.txt 2>&1) || true
-  [ -s /tmp/diff.txt ] || ([[ "${BRANCH}" != "master" && "${TRAVIS_EVENT_TYPE}" == "push" ]] || (echo "/tmp/diff.txt is empty for COMMIT_RANGE=$COMMIT_RANGE; try pulling base branch (often master) into compare branch (often your feature branch)" && false))
-  python /tmp/plume-scripts/lint-diff.py --guess-strip /tmp/diff.txt /tmp/warnings.txt
-fi
+/tmp/plume-scripts/ci-lint-diff /tmp/warnings.txt
