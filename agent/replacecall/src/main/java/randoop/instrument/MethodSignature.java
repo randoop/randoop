c00135cb68af27a601d99d449d6718fa8afe53d4
Michael Ernst <mernst@cs.washington.edu>
2019-07-21 10:49:12 -0700

    Add comment

--- agent/replacecall/src/main/java/randoop/instrument/MethodSignature.java
+++ agent/replacecall/src/main/java/randoop/instrument/MethodSignature.java
@@ -254,6 +254,8 @@ public class MethodSignature implements Comparable<MethodSignature> {
         // The current class is Object; the search completed without finding a matching method.
         throw new NoSuchMethodException("Method " + this.name + " not found");
       }
+      // Cannot use "currentClass = currentClass.getSuperClass()" because the superclass might
+      // not have been loaded into BCEL yet.
       currentClassname = currentClass.getSuperclassName();
     }
   }
