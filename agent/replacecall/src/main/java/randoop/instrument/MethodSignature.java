cb02f0de1e01328d2e304c7a42d6c915b917bdd3
Michael Ernst <mernst@cs.washington.edu>
2020-02-02 23:06:23 -0800

    Fix type-checking problems

--- agent/replacecall/src/main/java/randoop/instrument/MethodSignature.java
+++ agent/replacecall/src/main/java/randoop/instrument/MethodSignature.java
@@ -8,7 +8,6 @@ import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.ConstantPoolGen;
 import org.apache.bcel.generic.InvokeInstruction;
 import org.apache.bcel.generic.Type;
-import org.checkerframework.checker.signature.qual.BinaryName;
 import org.checkerframework.checker.signature.qual.FqBinaryName;
 import org.plumelib.bcelutil.BcelUtil;
 import org.plumelib.reflection.Signatures;
@@ -97,7 +96,8 @@ public class MethodSignature implements Comparable<MethodSignature> {
     String methodName = fullMethodName.substring(dotPos + 1);
     Type[] paramTypes = new Type[params.length];
     for (int i = 0; i < params.length; i++) {
-      paramTypes[i] = BcelUtil.classnameToType(params[i]);
+      // TODO: This seems incorrect, since it doesn't handle arrays.
+      paramTypes[i] = BcelUtil.fqBinaryNameToType(params[i]);
     }
 
     return new MethodSignature(classname, methodName, paramTypes);
@@ -128,12 +128,12 @@ public class MethodSignature implements Comparable<MethodSignature> {
     }
     String paramString = signature.substring(parenPos + 1, lastParenPos);
     @SuppressWarnings("signature:assignment.type.incompatible") // dynamically checked just below
-    @BinaryName String[] parameters =
+    @FqBinaryName String[] parameters =
         paramString.isEmpty() ? new String[0] : paramString.trim().split("\\s*,\\s*");
     for (String parameter : parameters) {
-      if (!Signatures.isBinaryName(parameter)) {
+      if (!Signatures.isFqBinaryName(parameter)) {
         throw new IllegalArgumentException(
-            "Bad parameter \"" + parameter + "\" in signature: " + signature);
+            "Bad parameter type \"" + parameter + "\" in signature: " + signature);
       }
     }
     return MethodSignature.of(fullMethodName, parameters);
