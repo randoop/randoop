5f1d4e996222a288fb51ef115f6066649c0175dd
Michael Ernst <mernst@cs.washington.edu>
2020-02-06 15:37:22 -0800

    Fix polarity

--- src/main/java/randoop/test/ValueSizePredicate.java
+++ src/main/java/randoop/test/ValueSizePredicate.java
@@ -4,7 +4,7 @@ import java.util.function.Predicate;
 import randoop.sequence.ExecutableSequence;
 import randoop.sequence.Value;
 
-/** Returns true if the sequences returns a too-large value. */
+/** Returns true if the sequence returns a value that is NOT too large value. */
 public class ValueSizePredicate implements Predicate<ExecutableSequence> {
 
   /** Creates a ValueSizePredicate. */
@@ -12,6 +12,6 @@ public class ValueSizePredicate implements Predicate<ExecutableSequence> {
 
   @Override
   public boolean test(ExecutableSequence eseq) {
-    return !Value.lastValueSizeOk(eseq);
+    return Value.lastValueSizeOk(eseq);
   }
 }
