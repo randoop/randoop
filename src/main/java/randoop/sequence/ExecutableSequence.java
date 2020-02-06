9660159ff608de7675f22c4e6d319f6f482f388c
Michael Ernst <mernst@cs.washington.edu>
2020-02-06 15:15:06 -0800

    Fix polarity of test

--- src/main/java/randoop/sequence/ExecutableSequence.java
+++ src/main/java/randoop/sequence/ExecutableSequence.java
@@ -339,7 +339,7 @@ public class ExecutableSequence {
 
       // Phase 2 of specification checking: check for expected behavior after the call.
       // This is the only client call to generateTestChecks().
-      if (!Value.lastValueSizeOk(this)) {
+      if (Value.lastValueSizeOk(this)) {
         Log.logPrintf(
             "Excluding from generateTestChecks due to value too large in last statement%n");
         checks = gen.generateTestChecks(this);
