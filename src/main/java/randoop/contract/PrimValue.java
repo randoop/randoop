6f36099b44b69eb35026ed04390e3fbad59248f6
Michael Ernst <mernst@alum.mit.edu>
2020-02-09 14:49:51 -0800

    Rename PrintMode to EqualityMode

--- src/main/java/randoop/contract/PrimValue.java
+++ src/main/java/randoop/contract/PrimValue.java
@@ -20,17 +20,21 @@ import randoop.types.TypeTuple;
  */
 public final class PrimValue extends ObjectContract {
 
-  /** Specifies how the contract is to be printed. */
-  public enum PrintMode {
+  /** Specifies what type of equality the contract uses. */
+  public enum EqualityMode {
+    /** Use reference equality {@code ==}. */
     EQUALSEQUALS,
+    /** Use abstract equality {@code .equals()}. */
     EQUALSMETHOD
   }
 
-  // The runtime value of the primitive value.
-  // Is a primitive or String (checked during construction).
+  /**
+   * The expected run-time value. It is a boxed primitive or String (checked during construction).
+   */
   public final Object value;
 
-  private final PrintMode printMode;
+  /** Whether to use {@code ==} or {@code .equals()} to test for equality. */
+  private final EqualityMode equalityMode;
 
   @Override
   public boolean equals(Object o) {
@@ -51,9 +55,9 @@ public final class PrimValue extends ObjectContract {
 
   /**
    * @param value the value for the expression: a primitive value or string
-   * @param printMode the print mode in which the check is written as an assertion
+   * @param equalityMode what equality test the assertion uses
    */
-  public PrimValue(Object value, PrintMode printMode) {
+  public PrimValue(Object value, EqualityMode equalityMode) {
     if (value == null) {
       throw new IllegalArgumentException("value cannot be null");
     }
@@ -66,7 +70,7 @@ public final class PrimValue extends ObjectContract {
       throw new StringTooLongException((String) value);
     }
     this.value = value;
-    this.printMode = printMode;
+    this.equalityMode = equalityMode;
   }
 
   @Override
@@ -120,7 +124,7 @@ public final class PrimValue extends ObjectContract {
       b.append(", ");
       b.append(Value.toCodeString(value));
       b.append(", 0);");
-    } else if (printMode.equals(PrintMode.EQUALSMETHOD)) {
+    } else if (equalityMode.equals(EqualityMode.EQUALSMETHOD)) {
       b.append("org.junit.Assert.assertTrue(");
       // First add a message
       b.append("\"'\" + " + "x0" + " + \"' != '\" + ")
@@ -133,7 +137,7 @@ public final class PrimValue extends ObjectContract {
       // Close assert.
       b.append(");");
     } else {
-      assert printMode.equals(PrintMode.EQUALSEQUALS);
+      assert equalityMode.equals(EqualityMode.EQUALSEQUALS);
       b.append("org.junit.Assert.assertTrue(");
       b.append("\"'\" + " + "x0" + " + \"' != '\" + ")
           .append(Value.toCodeString(value))
