c1be0266f4d013b93f12ba1a2f3bb90f37098f8a
Michael Ernst <mernst@cs.washington.edu>
2020-02-16 10:30:18 -0800

    Throw RandoopBug explicitly

--- src/main/java/randoop/util/ReflectionExecutor.java
+++ src/main/java/randoop/util/ReflectionExecutor.java
@@ -5,6 +5,7 @@ import org.plumelib.options.OptionGroup;
 import randoop.ExceptionalExecution;
 import randoop.ExecutionOutcome;
 import randoop.NormalExecution;
+import randoop.main.RandoopBug;
 
 /**
  * Static methods that executes the code of a ReflectionCode object.
@@ -164,9 +165,11 @@ public final class ReflectionExecutor {
     } catch (ThreadDeath e) { // can't stop these guys
       throw e;
     } catch (ReflectionCode.ReflectionCodeException e) { // bug in Randoop
-      throw e;
+      throw new RandoopBug("code=" + code, e);
     } catch (Throwable e) {
-      assert !(e instanceof java.lang.reflect.InvocationTargetException);
+      if (e instanceof java.lang.reflect.InvocationTargetException) {
+        throw new RandoopBug("Unexpected InvocationTargetException", e);
+      }
 
       // Debugging -- prints unconditionally, to System.out.
       // printExceptionDetails(e, System.out);
