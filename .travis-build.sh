3cf5261b96bbc26f4ba312b744cbaa419e245775
Michael Ernst <mernst@cs.washington.edu>
2020-02-18 12:19:25 -0800

    Run both Error Prone and the Checker Framework

--- .travis-build.sh
+++ .travis-build.sh
@@ -89,7 +89,6 @@ fi
 # fi
 
 if [[ "${GROUP}" == "misc" || "${GROUP}" == "all" ]]; then
-  ./gradlew clean assemble testClasses -PuseCheckerFramework=true
   ./gradlew javadoc
   ./gradlew checkstyle checkstyleMain checkstyleCoveredTest checkstyleReplacecallTest
   ./gradlew manual
