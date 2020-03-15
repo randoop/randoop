ebdb024552114a3b9c5192361ad391610af50b7b
Michael Ernst <mernst@cs.washington.edu>
2020-03-15 09:16:58 -0700

    Add more tests

--- src/test/resources/minimizer/TestInputWithPassingAssertionValue.java
+++ src/test/resources/minimizer/TestInputWithPassingAssertionValue.java
@@ -55,6 +55,30 @@ public class TestInputWithPassingAssertionValue {
     org.junit.Assert.assertTrue(a_squared * 2 >= incorrect_double_a_squared);
   }
 
+  @Test
+  public void test5() throws Throwable {
+    int j = 0;
+    // Orphan comment that belongs nowhere.
+    // Set j to 3.
+    // Additional comment.
+    j = 3;
+    // Orphan comment that should remain if statement is not removed.
+    // Assertion fails.
+    org.junit.Assert.assertEquals("They should be equal", j, 2);
+  }
+
+  @Test
+  public void test6() throws Throwable {
+    int j = 0;
+    // Orphan comment that belongs nowhere.
+    // Set j to 3.
+    // Additional comment.
+    j = 3;
+    // Orphan comment that should remain if statement is not removed.
+    // Assertion fails.
+    org.junit.Assert.assertEquals("They should be equal", 2, j);
+  }
+
   public static int incorrectDouble(int a) {
     if (a == 400) {
       return a * 4;
