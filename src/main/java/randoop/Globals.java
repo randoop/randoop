6f0c755c75ca071bb2cfde53851858f72a76d4d3
Michael Ernst <mernst@cs.washington.edu>
2020-03-31 22:22:09 -0700

    Update version to 4.2.3

--- src/main/java/randoop/Globals.java
+++ src/main/java/randoop/Globals.java
@@ -11,7 +11,7 @@ import randoop.main.RandoopBug;
 public class Globals {
 
   /** The version number for Randoop. */
-  public static final String RANDOOP_VERSION = "4.2.2";
+  public static final String RANDOOP_VERSION = "4.2.3";
 
   /** The system-specific line separator string. */
   public static final String lineSep = System.lineSeparator();
