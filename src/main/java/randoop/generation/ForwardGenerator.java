a82814b0ebb7c02f2c80c35142cc3cf4581e19ec
Michael Ernst <mernst@cs.washington.edu>
2021-04-21 21:02:39 -0700

    Use method references instead of lambdas

--- src/main/java/randoop/generation/ForwardGenerator.java
+++ src/main/java/randoop/generation/ForwardGenerator.java
@@ -476,8 +476,7 @@ public class ForwardGenerator extends AbstractGenerator {
     Sequence concatSeq = Sequence.concatenate(inputs.sequences);
 
     // Figure out input variables.
-    List<Variable> inputVars =
-        CollectionsPlume.mapList(inputIndex -> concatSeq.getVariable(inputIndex), inputs.indices);
+    List<Variable> inputVars = CollectionsPlume.mapList(concatSeq::getVariable, inputs.indices);
 
     Sequence newSequence = concatSeq.extend(operation, inputVars);
 
@@ -554,7 +553,7 @@ public class ForwardGenerator extends AbstractGenerator {
         }
       }
       Sequence currentRetval = retval;
-      List<Variable> vl = CollectionsPlume.mapList(vi -> currentRetval.getVariable(vi), vil);
+      List<Variable> vl = CollectionsPlume.mapList(currentRetval::getVariable, vil);
       retval = retval.extend(operation, vl);
     }
     return retval;
