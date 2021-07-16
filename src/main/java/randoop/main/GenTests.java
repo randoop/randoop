4ed720b5f5137798f55094d1486b44f5dc1ade0e
Ivan_Kocherhin <ivan_kocherhin@epam.com>
2021-07-16 15:45:01 +0300

    Delete fileSystemCache because it doesn't help when multiple GenTests objects are used

--- src/main/java/randoop/main/GenTests.java
+++ src/main/java/randoop/main/GenTests.java
@@ -13,6 +13,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystem;
+import java.nio.file.FileSystemAlreadyExistsException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -399,7 +400,7 @@ public class GenTests extends GenInputsAbstract {
       operationModel.dumpModel(System.out);
       System.out.println();
       System.out.println(NO_OPERATIONS_TO_TEST);
-      System.exit(1);
+      return false;
     }
     if (GenInputsAbstract.progressdisplay) {
       System.out.println("PUBLIC MEMBERS=" + operations.size());
@@ -1311,12 +1312,6 @@ public class GenTests extends GenInputsAbstract {
   }
 
   /**
-   * A cache used by {@link #getResourceDirectoryPath}, to prevent {@code
-   * FileSystemAlreadyExistsException}.
-   */
-  private Map<URI, FileSystem> fileSystemCache = new HashMap<>();
-
-  /**
    * Returns the path for the resource directory in the jar file.
    *
    * @param resourceDirectory the resource directory relative to the root of the jar file, should
@@ -1332,15 +1327,14 @@ public class GenTests extends GenInputsAbstract {
       throw new RandoopBug("Error locating directory " + resourceDirectory, e);
     }
 
-    FileSystem fileSystem = fileSystemCache.get(directoryURI);
-    if (fileSystem == null) {
-      try {
-        fileSystem =
-            FileSystems.newFileSystem(directoryURI, Collections.<String, Object>emptyMap());
-        fileSystemCache.put(directoryURI, fileSystem);
-      } catch (IOException e) {
-        throw new RandoopBug("Error locating directory " + resourceDirectory, e);
-      }
+    FileSystem fileSystem;
+    try {
+      fileSystem =
+          FileSystems.newFileSystem(directoryURI, Collections.<String, Object>emptyMap());
+    } catch (IOException e) {
+      throw new RandoopBug("Error locating directory " + resourceDirectory, e);
+    } catch (FileSystemAlreadyExistsException e) {
+      fileSystem = FileSystems.getFileSystem(directoryURI);
     }
 
     return fileSystem.getPath(resourceDirectory);
