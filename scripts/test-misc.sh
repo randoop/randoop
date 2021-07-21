4ad523b78b7786bca4fe324da6ea4b73714729d3
Michael Ernst <mernst@cs.washington.edu>
2021-07-21 13:49:42 -0700

    Fix incorrect Gradle task name

--- scripts/test-misc.sh
+++ scripts/test-misc.sh
@@ -21,5 +21,5 @@ if [ -d "/tmp/$USER/plume-scripts" ] ; then
 else
   mkdir -p "/tmp/$USER" && git -C "/tmp/$USER" clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
 fi
-(./gradlew requireJavadocPrivate > /tmp/warnings.txt 2>&1) || true
+(./gradlew requireJavadoc > /tmp/warnings.txt 2>&1) || true
 "/tmp/$USER/plume-scripts/ci-lint-diff" /tmp/warnings.txt
