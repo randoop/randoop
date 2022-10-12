d81b495ddb5d62e4d55579c7d29256b23028a668
Michael Ernst <mernst@cs.washington.edu>
2022-10-12 12:53:47 -0700

    Retry in case of network lossage

--- scripts/test-nonSystemTest.sh
+++ scripts/test-nonSystemTest.sh
@@ -8,7 +8,7 @@ set -o verbose
 set -o xtrace
 export SHELLOPTS
 
-./gradlew assemble
+./gradlew assemble || (sleep 1m && ./gradlew assemble)
 
 # Need GUI for running tests of replace call agent with Swing/AWT.
 # Run xvfb.
@@ -16,7 +16,8 @@ export DISPLAY=:99.0
 XVFB=/usr/bin/Xvfb
 XVFBARGS="$DISPLAY -ac -screen 0 1024x768x16 +extension RANDR"
 PIDFILE=/tmp/xvfb_${DISPLAY:1}.pid
-/sbin/start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
+# shellcheck disable=SC2086
+/sbin/start-stop-daemon --start --quiet --pidfile "$PIDFILE" --make-pidfile --background --exec "$XVFB" -- $XVFBARGS
 sleep 3 # give xvfb some time to start
 
 ./gradlew printJunitJarPath
