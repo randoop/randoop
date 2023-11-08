9a3d38b46b0b62db522c32517c0144a3d5ba9633
Kuro1Fury <skycoder0820@gmail.com>
2023-11-08 14:18:24 -0800

    finish sequenceInfo

--- src/main/java/randoop/reflection/ClassLiteralExtractor.java
+++ src/main/java/randoop/reflection/ClassLiteralExtractor.java
@@ -19,8 +19,10 @@ class ClassLiteralExtractor extends DefaultClassVisitor {
 
   private MultiMap<ClassOrInterfaceType, Sequence> literalMap;
 
-  private static Map<Sequence, Integer> sequenceFrequency = new HashMap<>();
+  /* Maps a sequence to information about the sequence. */
+  private static Map<Sequence, SequenceInfo> sequenceInfoMap = new HashMap<>();
 
+  /* The number of classes visited. */
   private int classCount;
 
   ClassLiteralExtractor(MultiMap<ClassOrInterfaceType, Sequence> literalMap) {
@@ -33,6 +35,7 @@ class ClassLiteralExtractor extends DefaultClassVisitor {
     MultiMap<Class<?>, NonreceiverTerm> constantMap = new MultiMap<>();
     ClassFileConstants.ConstantSet constantSet = ClassFileConstants.getConstants(c.getName());
     ClassFileConstants.buildConstantMap(constantSet, constantMap);
+    HashSet<Sequence> occurredSequences = new HashSet<>();
     for (Class<?> constantClass : constantMap.keySet()) {
       ClassOrInterfaceType constantType = ClassOrInterfaceType.forClass(constantClass);
       classCount++;
@@ -44,12 +47,65 @@ class ClassLiteralExtractor extends DefaultClassVisitor {
                     TypedOperation.createNonreceiverInitialization(term),
                     new ArrayList<Variable>(0));
         literalMap.add(constantType, seq);
-        // Update the global frequency for this sequence
-        sequenceFrequency.put(
+        updateSequenceInfo(
             seq,
-            sequenceFrequency.getOrDefault(seq, 0)
-                + constantSet.constantFrequency.get(term.getValue()));
+            constantType,
+            occurredSequences.contains(seq),
+            constantSet.constantFrequency.get(term.getValue()));
+        occurredSequences.add(seq);
       }
+    }
+  }
+
+  private void updateSequenceInfo(
+      Sequence seq, ClassOrInterfaceType type, Boolean hasOccurred, int frequency) {
+    Package pkg = type.getPackage();
+    SequenceInfo si = sequenceInfoMap.getOrDefault(seq, new SequenceInfo());
+    si.update(seq, type, pkg, hasOccurred, frequency);
+    sequenceInfoMap.put(seq, si);
+  }
+
+  static class SequenceInfo {
+    public int globalFrequency;
+    public int globalOccurrence;
+
+    /* How many times the sequence occur by the class */
+    public Map<ClassOrInterfaceType, Integer> classFrequency;
+
+    /* How many times the sequence occur by the package */
+    public Map<Package, Integer> packageFrequency;
+
+    /* How many classes the sequence occur by the package */
+    public Map<Package, Integer> packageOccurrence;
+
+    public SequenceInfo() {
+      globalFrequency = 0;
+      globalOccurrence = 0;
+      classFrequency = new HashMap<>();
+      packageFrequency = new HashMap<>();
+      packageOccurrence = new HashMap<>();
+    }
+
+    public void update(
+        Sequence seq, ClassOrInterfaceType type, Package pkg, boolean hasOccurred, int frequency) {
+      globalFrequency += frequency;
+      classFrequency.put(type, classFrequency.getOrDefault(type, 0) + frequency);
+      packageFrequency.put(pkg, packageFrequency.getOrDefault(pkg, 0) + frequency);
+      if (!hasOccurred) {
+        globalOccurrence++;
+        packageOccurrence.put(pkg, packageOccurrence.getOrDefault(pkg, 0) + 1);
+      }
+    }
+
+    @Override
+    public String toString() {
+      StringBuilder sb = new StringBuilder();
+      sb.append("globalFrequency: ").append(globalFrequency).append("\n");
+      sb.append("globalOccurrence: ").append(globalOccurrence).append("\n");
+      sb.append("classFrequency: ").append(classFrequency).append("\n");
+      sb.append("packageFrequency: ").append(packageFrequency).append("\n");
+      sb.append("packageOccurrence: ").append(packageOccurrence).append("\n");
+      return sb.toString();
     }
   }
 }
