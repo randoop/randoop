f8cbf54b9fe029f40cd64adf645f70cc3082ade3
776styjsu <yt776styjsu@gmail.com>
2024-03-16 01:46:57 -0700

    Improve documentation

--- src/main/java/randoop/contract/CompareToEqualsWithImpurity.java
+++ src/main/java/randoop/contract/CompareToEqualsWithImpurity.java
@@ -5,7 +5,11 @@ import randoop.types.JavaTypes;
 import randoop.types.TypeTuple;
 
 /**
- * The contract: Checks that compareTo is consistent with equals.
+ * The contract: Checks that compareTo is consistent with equals, except for StringBuilder.
+ * This contract is a special case of CompareToEquals to handle Impurity component from GRT as it
+ * uses StringBuilder to fuzz String inputs. However, StringBuilder does not override equals method
+ * while implementing Comparable interface. Hence, we need to ignore StringBuilder while checking
+ * compareToEquals contract.
  *
  * <pre>(x0.compareTo(x1) == 0) == x0.equals(x1)</pre>
  */
@@ -24,15 +28,7 @@ public class CompareToEqualsWithImpurity extends ObjectContract {
     Object o1 = objects[0];
     Object o2 = objects[1];
 
-    // System.out.println("o1: " + o1 + " " + o1.getClass()); -> java.lang.StringBuilder
-    // System.out.println("o2: " + o2 + " " + o2.getClass()); -> java.lang.StringBuilder
-    // System.out.println("is o1 comparable? " + (o1 instanceof Comparable)); -> true
-
-    // Hardcoded to ignore StringBuilder.
-    // This is because StringBuilder does not implement equals(), thus is using Object.equals().
-    // This causes the contract to fail.
-
-    // Actually, StringBuilder shouldn't be checked at all, because it is not a method under test.
+    // Ignore StringBuilder
     if (o1 instanceof Comparable && !(o1 instanceof StringBuilder)) {
       Comparable compObj1 = (Comparable) o1;
       return (compObj1.compareTo(o2) == 0) == o1.equals(o2);
