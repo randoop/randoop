9f4ebeb77a65135edb4736d9bcfcb5c938f67f34
Michael Ernst <mernst@cs.washington.edu>
2024-07-22 11:42:21 -0700

    Formatting

--- src/testInput/java/sideeffectfree/Square.java
+++ src/testInput/java/sideeffectfree/Square.java
@@ -1,27 +1,26 @@
 package sideeffectfree;
 
-
 import org.checkerframework.dataflow.qual.Pure;
 
 public class Square {
-    private int a, b;
+  private int a, b;
 
-    private Square() {
-        a = 2;
-        b = 3;
-    }
+  private Square() {
+    a = 2;
+    b = 3;
+  }
 
-    @Pure
-    public int getA() {
-        return a;
-    }
+  @Pure
+  public int getA() {
+    return a;
+  }
 
-    public static Square getNewSquare() {
-        return new Square();
-    }
+  public static Square getNewSquare() {
+    return new Square();
+  }
 
-    @Pure
-    public int getSum(Square other) {
-        return other.a + other.b + a + b;
-    }
+  @Pure
+  public int getSum(Square other) {
+    return other.a + other.b + a + b;
+  }
 }
