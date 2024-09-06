46a7e065e0ebd31a3ac674ff2d6fff465026f74f
Michael Ernst <mernst@cs.washington.edu>
2024-09-06 16:49:02 -0700

    Undo a change

--- src/main/java/randoop/util/ConstructorReflectionCode.java
+++ src/main/java/randoop/util/ConstructorReflectionCode.java
@@ -55,12 +55,12 @@ public final class ConstructorReflectionCode extends ReflectionCode {
       this.retval = this.constructor.newInstance(this.inputs);
     } catch (InvocationTargetException e) {
       // The underlying constructor threw an exception
-      this.exceptionThrown =
-          new Error(
-              String.format(
-                  "Failure in newInstance: constructor=%s, args=%s%n",
-                  this.constructor, Arrays.toString(this.inputs)),
-              e);
+      this.exceptionThrown = e.getCause();
+      // new Error(
+      //     String.format(
+      //         "Failure in newInstance: constructor=%s, args=%s%n",
+      //         this.constructor, Arrays.toString(this.inputs)),
+      //     e);
     } catch (Throwable e) {
       // Any other exception indicates Randoop should not have called the constructor
       throw new ReflectionCodeException(
