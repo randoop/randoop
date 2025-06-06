2f16270cb649e135869b1f15c3addf3d43afc69c
Michael Ernst <mernst@cs.washington.edu>
2025-06-05 19:45:50 -0700

    Test the Nullness Checker on changed lines in CI

--- scripts/test-misc.sh
+++ scripts/test-misc.sh
@@ -23,14 +23,13 @@ status=0
 make -C scripts style-check || status=1
 if grep -n -r --exclude-dir=test --exclude-dir=testInput --exclude="*~" '^\(import .*\*;$\)'; then
   echo "Don't use wildcard import"
-  status=2
+  status=1
 fi
 JAVA_VER=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1 | sed 's/-ea//')
 if [ "$JAVA_VER" != "8" ]; then
-  ./gradlew spotlessCheck || status=3
+  ./gradlew spotlessCheck || status=2
 fi
 
-## Javadoc documentation
 PLUME_SCRIPTS=/tmp/"$USER"/plume-scripts
 if [ -d "$PLUME_SCRIPTS" ]; then
   git -C "$PLUME_SCRIPTS" pull -q > /dev/null 2>&1
@@ -38,6 +37,12 @@ else
   PLUME_SCRIPTS_PARENT="$(dirname "$PLUME_SCRIPTS")"
   mkdir -p "$PLUME_SCRIPTS_PARENT" && git -C "$PLUME_SCRIPTS_PARENT" clone --depth=1 -q https://github.com/plume-lib/plume-scripts.git
 fi
+
+## Type-checking
+(./gradlew assemble -PcfNullness --console=plain --warning-mode=all --no-daemon > /tmp/warnings-nullness.txt 2>&1) || true
+"$PLUME_SCRIPTS"/ci-lint-diff /tmp/warnings-nullness.txt || status=3
+
+## Javadoc documentation
 (./gradlew requireJavadoc --console=plain --warning-mode=all --no-daemon > /tmp/warnings-rjp.txt 2>&1) || true
 "$PLUME_SCRIPTS"/ci-lint-diff /tmp/warnings-rjp.txt || status=4
 (./gradlew javadoc --console=plain --warning-mode=all --no-daemon > /tmp/warnings-javadoc.txt 2>&1) || true
