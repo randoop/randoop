0643cfcdceb9fbed7cf6318fc2326e246e64c29c
Michael Ernst <mernst@cs.washington.edu>
2025-06-18 12:36:22 -0700

    Comment tweaks

--- src/main/java/randoop/reflection/OperationModel.java
+++ src/main/java/randoop/reflection/OperationModel.java
@@ -128,6 +128,8 @@ public class OperationModel {
     operations = new TreeSet<>();
   }
 
+  // TODO: Much or all of this should be done in the constructor, rather than having a factory
+  // method.
   /**
    * Factory method to construct an operation model for a particular set of classes.
    *
@@ -482,6 +484,7 @@ public class OperationModel {
     return annotatedTestValues;
   }
 
+  /** Output the operations of this model, if logging is enabled. */
   public void log() {
     if (Log.isLoggingOn()) {
       logOperations(GenInputsAbstract.log);
