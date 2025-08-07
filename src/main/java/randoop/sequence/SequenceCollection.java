6e421a4019d8f78e1f21f74461a661be504bc771
Michael Ernst <mernst@cs.washington.edu>
2025-08-06 17:01:59 -0700

    Tweaks

--- src/main/java/randoop/sequence/SequenceCollection.java
+++ src/main/java/randoop/sequence/SequenceCollection.java
@@ -109,7 +109,7 @@ public class SequenceCollection {
    *
    * @param col the sequences to add
    */
-  public void addAll(Collection<Sequence> col) {
+  public void addAll(Collection<? extends Sequence> col) {
     for (Sequence s : col) {
       add(s);
     }
@@ -145,6 +145,7 @@ public class SequenceCollection {
    *
    * @param sequence the sequence to add to this collection
    */
+  @RequiresNonNull("this.sequenceMap")
   public void add(Sequence sequence) {
     List<Type> formalTypes = sequence.getTypesForLastStatement();
     List<Variable> arguments = sequence.getVariablesOfLastStatement();
