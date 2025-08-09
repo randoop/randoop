ef10e86efff47030191c85fad4c545b9979fb1d3
Michael Ernst <mernst@cs.washington.edu>
2025-08-09 10:09:47 -0700

    Add experimental `incrementNumsForSequence()`

--- src/main/java/randoop/generation/constanttfidf/ConstantStatistics.java
+++ src/main/java/randoop/generation/constanttfidf/ConstantStatistics.java
@@ -7,13 +7,99 @@ import java.util.Set;
 import randoop.sequence.Sequence;
 
 /**
- * This class stores information about constants used in part of the SUT. There is one {@code
- * ConstantStatistics} per "scope", where a scope is a class, a package, or the entire SUT.
+ * This class stores information about constants used in one "scope" of the SUT, where a scope is a
+ * class, a package, or the entire SUT.
  *
- * <p>It stores the number of uses of the sequence, the number of classes that contain the sequence,
- * and the total number of classes in the current scope.
+ * <p>For each constant (represented as a sequence), it stores the number of uses of the sequence
+ * and the number of classes that contain the sequence. It also stores the number of classes in the
+ * scope.
  */
 public class ConstantStatistics {
+
+  /** A map from a constant to its usage statistics. */
+  private Map<Sequence, ConstantUses> constantStats = new HashMap<>();
+
+  /** The number of classes in this scope. */
+  private int numClasses = 0;
+
+  /** Creates a new empty ConstantStatistics. */
+  public ConstantStatistics() {}
+
+  /**
+   * Returns the map from constant to its usage statistics.
+   *
+   * @return the map from constant to its usage statistics
+   */
+  public Map<Sequence, ConstantUses> getConstantUses() {
+    return constantStats;
+  }
+
+  /**
+   * Returns the number of classes in the current scope.
+   *
+   * @return the number of classes in the current scope
+   */
+  public Integer getNumClasses() {
+    return numClasses;
+  }
+
+  /**
+   * Records a constant sequence.
+   *
+   * @param seq a sequence
+   * @param numUses the number of uses of the sequence
+   * @param numClassesWith the number of classes that contain the sequence
+   */
+  public void incrementNumsForSequence(Sequence seq, int numUses, int numClassesWith) {
+    ConstantUses currentStats = constantStats.getOrDefault(seq, new ConstantUses(0, 0));
+    constantStats.put(
+        seq,
+        new ConstantUses(
+            currentStats.getNumUses() + numUses,
+            currentStats.getNumClassesWith() + numClassesWith));
+  }
+
+  /**
+   * Increments the number of uses.
+   *
+   * @param seq a sequence
+   * @param num the number of uses of the sequence
+   */
+  public void incrementNumUses(Sequence seq, int num) {
+    ConstantUses currentStats = constantStats.getOrDefault(seq, new ConstantUses(0, 0));
+    constantStats.put(
+        seq, new ConstantUses(currentStats.getNumUses() + num, currentStats.getNumClassesWith()));
+  }
+
+  /**
+   * Increments the number of classes that contain a sequence.
+   *
+   * @param seq a sequence
+   * @param num the number of classes that contain the sequence
+   */
+  public void incrementNumClassesWith(Sequence seq, int num) {
+    ConstantUses currentStats = constantStats.getOrDefault(seq, new ConstantUses(0, 0));
+    constantStats.put(
+        seq, new ConstantUses(currentStats.getNumUses(), currentStats.getNumClassesWith() + num));
+  }
+
+  /**
+   * Increments the number of classes.
+   *
+   * @param num the number of classes to add to the current total
+   */
+  public void incrementNumClasses(int num) {
+    numClasses += num;
+  }
+
+  /**
+   * Returns all sequences that have been recorded.
+   *
+   * @return the sequences that have been recorded
+   */
+  public Set<Sequence> getSequenceSet() {
+    return new HashSet<>(constantStats.keySet());
+  }
 
   /**
    * Statistics for one constant within one scope: the number of uses of the constant and the number
@@ -54,74 +140,5 @@ public class ConstantStatistics {
     public int getNumClassesWith() {
       return numClassesWith;
     }
-  }
-
-  /** A map from a constant to its usage statistics. */
-  private Map<Sequence, ConstantUses> constantStats = new HashMap<>();
-
-  /** The number of classes in this scope. */
-  private int numClasses = 0;
-
-  /** Creates a new empty ConstantStatistics. */
-  public ConstantStatistics() {}
-
-  /**
-   * Returns the map from constant to its usage statistics.
-   *
-   * @return the map from constant to its usage statistics
-   */
-  public Map<Sequence, ConstantUses> getConstantUses() {
-    return constantStats;
-  }
-
-  /**
-   * Returns the number of classes in the current scope.
-   *
-   * @return the number of classes in the current scope
-   */
-  public Integer getNumClasses() {
-    return numClasses;
-  }
-
-  /**
-   * Increments the number of uses.
-   *
-   * @param seq a sequence
-   * @param num the number of uses of the sequence
-   */
-  public void incrementNumUses(Sequence seq, int num) {
-    ConstantUses currentStats = constantStats.getOrDefault(seq, new ConstantUses(0, 0));
-    constantStats.put(
-        seq, new ConstantUses(currentStats.getNumUses() + num, currentStats.getNumClassesWith()));
-  }
-
-  /**
-   * Increments the number of classes that contain the sequence.
-   *
-   * @param seq the sequence to be added
-   * @param num the number of classes that contain the sequence to be added
-   */
-  public void incrementNumClassesWith(Sequence seq, int num) {
-    ConstantUses currentStats = constantStats.getOrDefault(seq, new ConstantUses(0, 0));
-    constantStats.put(
-        seq, new ConstantUses(currentStats.getNumUses(), currentStats.getNumClassesWith() + num));
-  }
-
-  /**
-   * Increments the number of classes.
-   *
-   * @param num the number of classes to add to the current total
-   */
-  public void incrementNumClasses(int num) {
-    numClasses += num;
-  }
-
-  /**
-   * Returns all sequences that have been recorded.
-   *
-   * @return the set of sequences that have been recorded
-   */
-  public Set<Sequence> getSequenceSet() {
-    return new HashSet<>(constantStats.keySet());
   }
 }
