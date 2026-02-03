816e211b0e7d96ca8322de9ef59d80740e29c438
Michael Ernst <mernst@cs.washington.edu>
2026-02-02 16:37:54 -0800

    Fix

--- src/systemTest/java/randoop/main/CoverageChecker.java
+++ src/systemTest/java/randoop/main/CoverageChecker.java
@@ -321,7 +321,7 @@ class CoverageChecker {
           include(methodName);
           break;
         default:
-          throw new Error("Unrecognized action " + action + " in method spec: " + s);
+          throw new Error("Unrecognized action " + action + " in method spec");
       }
     }
   }
