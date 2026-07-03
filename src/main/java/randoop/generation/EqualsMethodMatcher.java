1d8b62aec98dcc29419f3064501176543d1fb61a
Michael Ernst <mernst@cs.washington.edu>
2026-07-02 19:40:49 -0700

    Add comment

--- src/main/java/randoop/generation/EqualsMethodMatcher.java
+++ src/main/java/randoop/generation/EqualsMethodMatcher.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 public class EqualsMethodMatcher implements StateMatcher {
 
+  /** A cache. */
   private final Set<Object> cache = new LinkedHashSet<>();
 
   /** Creates an EqualsMethodMatcher. */
