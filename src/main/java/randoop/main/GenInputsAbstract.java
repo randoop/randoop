b3ef5bce3690a624d09914be988dbc30435ea5a2
Michael Ernst <mernst@cs.washington.edu>
2026-07-27 07:56:21 -0700

    Documentation fix

--- src/main/java/randoop/main/GenInputsAbstract.java
+++ src/main/java/randoop/main/GenInputsAbstract.java
@@ -600,7 +600,7 @@ public abstract class GenInputsAbstract extends CommandHandler {
    * <p>For example, a null ratio of 0.05 directs Randoop to use {@code null} as an input 5 percent
    * of the time when a non-{@code null} value of the appropriate type is available.
    *
-   * <p>Unless {@code --forbid_null} is supplied, a {@code null} value will still be used if no
+   * <p>Unless {@code --forbid-null} is supplied, a {@code null} value will still be used if no
    * other value can be passed as an argument even if {@code --null-ratio=0}.
    *
    * <p>Randoop never uses {@code null} for receiver values.
